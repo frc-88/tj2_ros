reworked plotting. Found critical bug in forward kinematics calculation. vx and vy were reversed in module state matrix

playground/swerve-kinematics/module_states.py
@@ -6,9 +6,12 @@ class ModuleStates:
         self.num_modules = num_modules
         self.state = np.zeros((num_modules, 2))
     
-    def set(self, index, wheel_speed, azimuth):
+    def set(self, index, azimuth, wheel_speed):
         self.state[index][0] = azimuth
         self.state[index][1] = wheel_speed
+    
+    def get(self, index):
+        return self.state[index][0], self.state[index][1]
     
     @classmethod
     def from_state(cls, state):