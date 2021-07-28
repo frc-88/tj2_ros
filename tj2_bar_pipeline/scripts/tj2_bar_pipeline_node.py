af662c097a0eff6d686787c886df03fd2780a59a
Woz4tetra <woz4tetra@gmail.com>
2021-07-28 04:04:36 -0400

    lots of silly fixes. Pipeline is running and returning sensible data

--- tj2_bar_pipeline/scripts/tj2_bar_pipeline_node.py
+++ tj2_bar_pipeline/scripts/tj2_bar_pipeline_node.py
@@ -8,10 +8,12 @@ import numpy as np
 
 import tf2_ros
 import tf_conversions
+import tf2_geometry_msgs
 
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 
+from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
 
@@ -39,7 +41,7 @@ class TJ2BarPipeline(object):
         )
         self.bridge = CvBridge()
 
-        self.min_distance = rospy.get_param("~min_distance", 1.5)
+        self.min_distance = rospy.get_param("~min_distance", 0.5)
         self.max_distance = rospy.get_param("~max_distance", 5.0)
         self.contour_perimeter_threshold = rospy.get_param("~contour_perimeter_threshold", 200)
         self.line_angle_lower_threshold = rospy.get_param("~line_angle_lower_threshold", -math.pi / 4)
@@ -49,6 +51,13 @@ class TJ2BarPipeline(object):
         self.hough_lines_threshold = rospy.get_param("~hough_lines_threshold", 100)
         self.hough_lines_min_length = rospy.get_param("~hough_lines_min_length", 100)
         self.hough_lines_max_gap = rospy.get_param("~hough_lines_max_gap", 100)
+        self.bar_z_lower_threshold = rospy.get_param("~bar_z_lower_threshold", 1.0)
+        self.bar_z_upper_threshold = rospy.get_param("~bar_z_upper_threshold", 5.0)
+
+        self.roi_left = rospy.get_param("~roi_left", 0)
+        self.roi_top = rospy.get_param("~roi_top", 20)
+        self.roi_right = rospy.get_param("~roi_right", 0)
+        self.roi_bottom = rospy.get_param("~roi_bottom", 20)
 
         self.depth_topic = rospy.get_param("~depth_topic", "depth/image_raw")
         self.info_topic = rospy.get_param("~info_topic", "depth/camera_info")
@@ -59,8 +68,13 @@ class TJ2BarPipeline(object):
 
         self.camera_model = None
         self.camera_frame = None
+        self.camera_info = None
+
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         
-        self.debug_image_pub = rospy.Publisher("pipeline_debug", Image, queue_size=1)
+        self.debug_image_pub = rospy.Publisher("pipeline_debug/image_raw", Image, queue_size=1)
+        self.debug_info_pub = rospy.Publisher("pipeline_debug/camera_info", CameraInfo, queue_size=1)
         self.bar_marker_pub = rospy.Publisher("bar_markers", MarkerArray, queue_size=10)
         
         rospy.Subscriber(self.depth_topic, Image, self.depth_callback, queue_size=1)
@@ -78,9 +92,10 @@ class TJ2BarPipeline(object):
         self.camera_model.fromCameraInfo(msg)
 
         self.camera_frame = msg.header.frame_id
+        self.camera_info = msg
 
     def depth_callback(self, msg):
-        if self.camera_model is not None:
+        if self.camera_model is None:
             return
 
         try:
@@ -89,7 +104,7 @@ class TJ2BarPipeline(object):
             rospy.logerr(e)
             return
         
-        bars, debug_image = self.pipeline(image, self.debug_image_pub.get_num_connections() > 0)
+        bars, debug_image = self.pipeline(cv2_img, self.debug_image_pub.get_num_connections() > 0)
 
         self.publish_bars(bars)
         self.publish_bar_visualization(bars)
@@ -101,11 +116,17 @@ class TJ2BarPipeline(object):
         threshold, depth_bounded = cv2.threshold(depth_bounded, self.min_distance_mm, 65535, cv2.THRESH_TOZERO)
 
         # convert to 0..255 range so OpenCV algorithms can process it
-        normalized = cv2.normalize(depth_bounded, depth_bounded, 0, 255, cv2.NORM_MINMAX)
-        normalized = np.uint8(normalized)
+        # normalized = cv2.normalize(depth_bounded, None, 0, 255, cv2.NORM_MINMAX)
+        normalized = np.uint8(depth_bounded.astype(np.float64) * 255 / self.max_distance_mm)
         if debug:
             debug_image = np.copy(normalized)
             debug_image = cv2.cvtColor(debug_image, cv2.COLOR_GRAY2BGR)
+            cv2.rectangle(
+                debug_image,
+                (self.roi_left, self.roi_top),
+                (depth_image.shape[1] - self.roi_right, depth_image.shape[0] - self.roi_bottom),
+                (0, 0, 255), 2
+            )
         else:
             debug_image = None
         
@@ -113,47 +134,32 @@ class TJ2BarPipeline(object):
         normalized = cv2.medianBlur(normalized, 3)
 
         # find contours and generate an image from them
-        contours_image, contours = self.contours(normalize)
+        contours_image, contours = self.contours(normalized)
 
         # identify lines in the image
         lines, debug_image = self.houghlines(contours_image, debug_image)
-        clouds = self.hough_line_clouds(depth_image, lines)
-
-        bars = []  # an array of potential bars in the robot's frame
-        for cloud in clouds:
-            bar_coordinates = []
-            for point in self.best_fit_3d(cloud):
-                camera_z = point[2] / 1000.0  # Z values in the point cloud are in mm
-                result = self.pixels_to_camera_frame(point[0], point[1], camera_z)
-                if result is None:
-                    rospy.logwarn("Unable to translate pixel coordinates to camera coordinates. No camera model has been received.")
-                    break
-                camera_x, camera_y = result
-
-                result = self.camera_to_robot_frame(camera_x, camera_y, camera_z)
-                if result is None:
-                    rospy.logwarn("Unable to translate camera coordinates to robot frame. TF from camera to robot has been received.")
-                    break
-                robot_x, robot_y, robot_z = result
-
-                bar_coordinates.append((robot_x, robot_y, robot_z))
-            bars.append(bar_coordinates)
+        clouds = self.hough_line_clouds(depth_bounded, lines)
+        bars = self.bars_from_cloud(lines, clouds)
         
         return bars, debug_image
 
-    def publish_debug_image(self):
-        if debug_image is not None:
-            try:
-                image_msg = bridge.cv2_to_imgmsg(debug_image, "bgr8")
-            except CvBridgeError as e:
-                print(e)
-            else:
-                self.debug_image_pub.publish(image_msg)
+    def publish_debug_image(self, debug_image):
+        if debug_image is None:
+            return
+        try:
+            image_msg = self.bridge.cv2_to_imgmsg(debug_image, "bgr8")
+            image_msg.header.frame_id = self.camera_frame
+        except CvBridgeError as e:
+            rospy.logerr(e)
+        else:
+            self.debug_image_pub.publish(image_msg)
+            self.debug_info_pub.publish(self.camera_info)
 
     def publish_bars(self, bars):
         pass
     
     def publish_bar_visualization(self, bars):
+        rospy.loginfo("%s potential bars in view" % len(bars))
         markers_msg = MarkerArray()
 
         for index, points in enumerate(bars):
@@ -162,21 +168,64 @@ class TJ2BarPipeline(object):
             marker.type = Marker.LINE_STRIP
             marker.ns = "bar"
             marker.id = index
+            marker.header.frame_id = self.base_link_frame
+            # marker.header.frame_id = self.camera_frame
+            marker.pose = Pose()
+            marker.pose.orientation.w = 1.0
+
+            marker.scale.x = 0.025  # line width
+            marker.color = ColorRGBA(0.5, 0.5, 1.0, 1.0)
+            marker.lifetime = rospy.Duration(0.5)
+
             for point in points:
                 msg_point = Point()
                 msg_point.x = point[0]
                 msg_point.y = point[1]
                 msg_point.z = point[2]
                 marker.points.append(msg_point)
-            marker.scale.x = 0.1  # line width
-            marker.color = ColorRGBA(1.0, 1.0, 1.0, 1.0)
+            
+            markers_msg.markers.append(marker)
 
         self.bar_marker_pub.publish(markers_msg)
 
+    def bars_from_cloud(self, lines, clouds):
+        bars = []  # an array of potential bars in the robot's frame
+        assert len(lines) == len(clouds)
+        for index in range(len(lines)):
+            cloud = clouds[index]
+            line = lines[index]
+            bar_coordinates = []
+            for point in self.best_fit_3d(line, cloud):
+                if point is None:
+                    break
+                camera_z = point[2] / 1000.0  # Z values in the point cloud are in mm
+                result = self.pixels_to_camera_frame(point[0], point[1], camera_z)
+                if result is None:
+                    rospy.logwarn("Unable to translate pixel coordinates to camera coordinates. No camera model has been received.")
+                    break
+                camera_x, camera_y = result
+                # print("camera: \t%0.4f\t%0.4f\t%0.4f" % (camera_x, camera_y, camera_z))
 
-    def best_fit_3d(self, cloud, z_outlier_stddev=1.5):
+                result = self.camera_to_robot_frame(camera_x, camera_y, camera_z)
+                if result is None:
+                    rospy.logwarn("Unable to translate camera coordinates to robot frame. TF from camera to robot has been received.")
+                    break
+                robot_x, robot_y, robot_z = result
+
+                if not (self.bar_z_lower_threshold <= robot_z <= self.bar_z_upper_threshold):
+                    break
+
+                bar_coordinates.append((robot_x, robot_y, robot_z))
+                # bar_coordinates.append((camera_x, camera_y, camera_z))
+            if len(bar_coordinates) == 2:
+                bars.append(bar_coordinates)
+        return bars
+
+    def best_fit_3d(self, line, cloud, z_outlier_stddev=1.5):
         # given a 3d cloud of points, return two 3d points that describes the best
         # fit line.
+
+        x1, y1, x2, y2 = line
 
         z_cloud = cloud[:, 2]
         z_std = z_cloud.std()
@@ -187,7 +236,7 @@ class TJ2BarPipeline(object):
 
         xy_fit = np.polyfit(cloud[:, 0], cloud[:, 1], 1)
         xz_fit = np.polyfit(cloud[:, 0], cloud[:, 2], 1)
-        
+
         y1_fit = xy_fit[0] * x1 + xy_fit[1]
         y2_fit = xy_fit[0] * x2 + xy_fit[1]
         z1_fit = xz_fit[0] * x1 + xz_fit[1]
@@ -203,7 +252,7 @@ class TJ2BarPipeline(object):
         # a 2D array describing the point cloud is returned
         nonzero_mask = depth_image > 0
         nonzero_mask = np.uint8(nonzero_mask)
-
+        
         clouds = []
         for x1, y1, x2, y2 in lines:
             mask_image = np.zeros_like(nonzero_mask)
@@ -219,22 +268,24 @@ class TJ2BarPipeline(object):
             cloud = []
             for xn in range(x1, x2):
                 zn = z_values[xn]
+                if not (self.min_distance_mm <= zn < self.max_distance_mm):
+                    continue
                 yn = (y2 - y1) / (x2 - x1) * (xn - x1) + y1
                 cloud.append((xn, yn, zn))
             if len(cloud) == 0:
                 continue
-            clouds.append(cloud)
+            clouds.append(np.array(cloud))
         return clouds
         
     def pixels_to_camera_frame(self, pixel_x, pixel_y, camera_z):
-        if self.camera_model is not None:
+        if self.camera_model is None:
             return None
         ray = self.camera_model.projectPixelTo3dRay((pixel_x, pixel_y))
         camera_x = ray[0] * camera_z
         camera_y = ray[1] * camera_z
         return camera_x, camera_y
     
-    def camera_to_robot_frame(self, camera_x, camera_y, camera_z):
+    def camera_to_robot_frame(self, camera_x, camera_y, camera_z, time_window=None, timeout=None):
         if time_window is None:
             time_window = rospy.Time(0)
         else:
@@ -244,9 +295,9 @@ class TJ2BarPipeline(object):
             timeout = rospy.Duration(1.0)
 
         try:
-            transform = self.tf_buffer.lookup_transform(self.camera_frame, self.base_link, time_window, timeout)
+            transform = self.tf_buffer.lookup_transform(self.base_link_frame, self.camera_frame, time_window, timeout)
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as e:
-            rospy.logwarn("Failed to look up %s to %s. %s" % (self.camera_frame, self.base_link, e))
+            rospy.logwarn("Failed to look up %s to %s. %s" % (self.camera_frame, self.base_link_frame, e))
             return None
 
         camera_pose = PoseStamped()
@@ -289,7 +340,7 @@ class TJ2BarPipeline(object):
         # image = cv2.Canny(image, 50, 150, apertureSize=3)
         contours = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contour_image = np.zeros_like(image)
-        contours = grab_contours(contours)
+        contours = self.grab_contours(contours)
         for contour in contours:
             perimeter = cv2.arcLength(contour, True)
             if perimeter < self.contour_perimeter_threshold:
@@ -313,6 +364,16 @@ class TJ2BarPipeline(object):
             angle = math.atan2(y2 - y1, x2 - x1)
             if not (self.line_angle_lower_threshold < angle < self.line_angle_upper_threshold):
                 continue
+
+            if x1 < self.roi_left and x2 < self.roi_left:
+                continue
+            if x1 > edges.shape[1] - self.roi_right and x2 > edges.shape[1] - self.roi_right:
+                continue
+            if y1 < self.roi_top and y2 < self.roi_top:
+                continue
+            if y1 > edges.shape[0] - self.roi_bottom and y2 > edges.shape[0] - self.roi_bottom:
+                continue
+            
             result.append((x1, y1, x2, y2))
             if debug_image is not None:
                 cv2.line(debug_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
