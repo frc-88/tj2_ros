a6b9aff6142725179120336e190b24809999bec3
Woz4tetra <woz4tetra@gmail.com>
2021-09-23 01:12:06 -0400

    adjusting installation processes

--- install/source_installation/06a_apply_patches.sh
+++ install/source_installation/06a_apply_patches.sh
@@ -1,13 +1,13 @@
-return
+# return
 
 BASE_DIR=$(realpath "$(dirname $0)")
 
 DEPENDENCIES_WS=$HOME/packages_ros_ws
 DEPENDENCIES_WS_SRC=${DEPENDENCIES_WS}/src
 
-find ${DEPENDENCIES_WS_SRC} -type f -name CMakeLists.txt -exec sed -i'' -e 's/Boost REQUIRED python37/Boost REQUIRED python36/g' {} +
+find ${DEPENDENCIES_WS_SRC} -type f -name CMakeLists.txt -exec sed -i'' -e 's/Boost REQUIRED python37/Boost REQUIRED python3/g' {} +
 
-find ${DEPENDENCIES_WS_SRC} -type f -name CMakeLists.txt -exec sed -i'' -e 's/find_package(realsense2 2.45.0)/find_package(realsense2 2.41.0)/g' {} +
+# find ${DEPENDENCIES_WS_SRC} -type f -name CMakeLists.txt -exec sed -i'' -e 's/find_package(realsense2 2.45.0)/find_package(realsense2 2.41.0)/g' {} +
 
 sed -i -e 's/${G2O_INCREMENTAL_LIB}/#${G2O_INCREMENTAL_LIB}/g'  ${DEPENDENCIES_WS_SRC}/teb_local_planner/cmake_modules/FindG2O.cmake
 
