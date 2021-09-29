cda6afbd0ea74e6f8d8377769a28b497e2898a40
Woz4tetra <woz4tetra@gmail.com>
2021-09-28 20:02:45 -0400

    found a setup for dynamic reconfigure that works

--- playground/networktable_tester/main.py
+++ playground/networktable_tester/main.py
@@ -6,27 +6,22 @@ class DummyServer:
         NetworkTables.initialize()
         assert NetworkTables.isServer()
 
-        self.nt = NetworkTables.getTable("coprocessor")
+        self.nt = NetworkTables.getTable("swerveLibrary")
 
-        self.driver_station_table_key = "host/DriverStation"
-        self.odom_table_key = "host/odom"
+        self.config_key = "configuration/modules"
 
         self.start_time = time.time()
     
     def run(self):
         while True:
-            self.nt.getEntry("host/timestamp").setDouble(self.server_time() * 1E6)
+            for index in range(4):
+                table = self.nt.getSubTable(self.config_key + "/" + str(index))
+                table.getEntry("azimuthController/iMax").setDouble(72)
 
-            self.publish_fms()
-
-            time.sleep(0.02)
+            time.sleep(0.5)
         
     def server_time(self):
         return time.time() - self.start_time
-    
-    def publish_fms(self):
-        self.nt.getEntry(self.driver_station_table_key + "/isFMSAttached").setBoolean(True)
-        self.nt.getEntry(self.driver_station_table_key + "/getMatchTime").setDouble(self.server_time())
 
 
 DummyServer().run()
