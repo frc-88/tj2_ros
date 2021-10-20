851c3222e7f8c20fc2317ff0564e73f59adc8a04
Woz4tetra <woz4tetra@gmail.com>
2021-10-19 22:26:40 -0400

    fixing env script to protect against time jumps. Small modifications to tj2_networktables and socket test

--- playground/socket_test/main.py
+++ playground/socket_test/main.py
@@ -3,7 +3,7 @@
 import socket
 import time
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+HOST = '10.0.88.2'  # The server's hostname or IP address
 PORT = 8080        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
