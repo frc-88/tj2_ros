f7b54b581e5105ba2d4e26f26d5979d7a903bb73
Woz4tetra <woz4tetra@gmail.com>
2021-10-20 10:33:04 -0400

    saving changes before nuking everything

--- playground/socket_test/protocol_client.py
+++ playground/socket_test/protocol_client.py
@@ -28,6 +28,8 @@ class TunnelClient:
         self.should_stop = False
         self.device_paused = False
 
+        self.read_buffer = b''
+
         self.read_task = threading.Thread(target=self.read_task_fn)
         self.read_thread_exception = None
 
@@ -38,22 +40,15 @@ class TunnelClient:
 
         self.prev_packet_num_report_time = 0.0
 
-        self.write_lock = threading.Lock()
-        self.read_lock = threading.Lock()
-
         self.ready_state = {
             "name": "",
             "is_ready": False,
             "time": 0.0
         }
 
-        self.read_buffer = b''
-
-        self.initial_connect_delay = 2.0
         self.check_ready_timeout = 10.0
-        self.write_timeout = 1.0
-        self.packet_read_timeout = 10.0
-        self.packet_ok_timeout = 10.0
+        self.packet_write_timeout = 1.0
+        self.packet_read_timeout = 1.0
 
         self.ready_keyword = "swerve"
 
@@ -62,20 +57,18 @@ class TunnelClient:
 
         self.is_active = False
 
-        self.wait_for_ok_reqs = {}
-
-        self.large_packets = {}
+        self.inputs = []
+        self.outputs = []
 
     def start(self):
         logger.info("Starting socket interface")
 
         self.device = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.device.connect((self.address, self.port))
-        time.sleep(self.initial_connect_delay)
+        self.inputs.append(self.device)
 
         self.read_task.start()
         logger.info("Read thread started")
-        time.sleep(1.0)
 
         self.check_ready()
 
@@ -120,13 +113,13 @@ class TunnelClient:
         while not self.ready_state["is_ready"]:
             if time.time() - begin_time > self.check_ready_timeout:
                 break
-            if time.time() - write_time > self.write_timeout:
+            if time.time() - write_time > self.packet_write_timeout:
                 logger.info("Writing ready signal again")
                 self.write("?", self.ready_keyword)
                 write_time = time.time()
 
-            if self.is_available():
-                self.read()
+            self.poll_socket()
+            self.read_packet()
 
         if self.ready_state["is_ready"]:
             self.set_start_time(self.ready_state["time"])
@@ -164,68 +157,22 @@ class TunnelClient:
         time.sleep(0.1)
 
         self.write_stop()
-
-        with self.read_lock:
-            self.device.close()
+        self.device.close()
         logger.info("Device connection closed")
 
     def pre_stop_callback(self):
         pass
 
-    def wait_for_ok(self, packet_num=None):
-        if packet_num is None:
-            packet_num = self.write_packet_num - 1
-        self.wait_for_ok_reqs[packet_num] = None
-        start_timer = time.time()
-        while True:
-            if time.time() - start_timer > self.packet_ok_timeout:
-                logger.warn("Timed out while waiting for response from packet #%s" % packet_num)
-                return False
-            if self.wait_for_ok_reqs[packet_num] is not None:
-                error_code = self.wait_for_ok_reqs.pop(packet_num)
-                logger.debug("Received response for packet #%s: %s" % (packet_num, error_code))
-                return error_code == 0 or error_code == 6
-            time.sleep(0.01)
-
-    def is_available(self):
-        readable, writable, exceptional = select.select([self.device], [], [])
-        for stream in readable:
-            if stream is self.device:
-                return True
-        return False
-        
     def write_txrx(self, code):
         self.write("txrx", self.protocol.read_packet_num, code)
 
-    def write_large(self, name, destination, arg):
-        assert type(arg) == str or type(arg) == bytes
-        if type(arg) == str:
-            arg = arg.encode()
-
-        segments = []
-        for index in range(0, len(arg), self.max_segment_len):
-            offset = min(self.max_segment_len, len(arg) - index)
-            segments.append(arg[index: index + offset])
-        num_segments = len(segments)
-        for index, segment in enumerate(segments):
-            self.write(name, destination, index, num_segments, segment)
-            if not self.wait_for_ok():
-                logger.warn("Failed to receive ok signal on segment %s of %s. %s" % (index + 1, num_segments, name))
-                break
-            else:
-                logger.info("Uploaded segment #%s of %s" % (index + 1, num_segments))
-
     def write(self, category, *args):
-        self.write_lock.acquire()
-
         if self.device_paused:
-            logger.debug("Device is paused. Skipping write: %s, %s" % (str(name), str(args)))
+            logger.debug("Device is paused. Skipping write: %s, %s" % (str(category), str(args)))
             return
 
         packet = self.protocol.make_packet(category, *args)
         self._write_packet(packet)
-
-        self.write_lock.release()
 
     def _write_packet(self, packet):
         logger.debug("Writing %s" % str(packet))
@@ -234,70 +181,15 @@ class TunnelClient:
         except BaseException as e:
             logger.error("Exception while writing packet %s: %s" % (packet, str(e)), exc_info=True)
 
-    def wait_for_packet_start(self):
-        begin_time = time.time()
-        msg_buffer = b""
-        c1 = ""
-        c2 = ""
-
-        while True:
-            if time.time() - begin_time > self.packet_read_timeout:
-                return False
-
-            if not self.is_available():
-                continue
-
-            c1 = self.device.recv(1)
-            if c1 == self.protocol.PACKET_START_0:
-                c2 = self.device.recv(1)
-                if c2 == self.protocol.PACKET_START_1:
-                    return True
-            elif c1 == self.protocol.PACKET_STOP:
-                logger.info("Device message: %s" % (msg_buffer))
-                msg_buffer = b""
-            else:
-                msg_buffer += c1
-
-    def read(self):
-        with self.read_lock:
-            return self._read()
-
-    def readline(self):
-        begin_time = time.time()
-        buffer = b""
-        len_bytes = b''
-        packet_len = 0
-        counter = 0
-        while True:
-            if time.time() - begin_time > self.packet_read_timeout:
-                break
-            if not self.is_available():
-                continue
-            c = self.device.recv(1)
-            if len(len_bytes) < 2:
-                len_bytes += c
-                if len(len_bytes) == 2:
-                    packet_len = int.from_bytes(len_bytes, "big")
-                continue
-
-            counter += 1
-            if counter > packet_len:
-                if c != self.protocol.PACKET_STOP:
-                    logger.error("Packet didn't end with stop character: %s. buffer: %s" % (c, buffer))
-                    return None
-                break
-            buffer += c
-        return buffer
-
-    def _read(self):
+    def read_packet(self):
         # if self.device_paused:
         #     logger.debug("Device is paused. Skipping read")
         #     return
 
-        if not self.wait_for_packet_start():
-            return
-
         buffer = self.readline()
+        logger.debug("Buffer: %s" % buffer)
+        if len(buffer) == 0:
+            return
         code, recv_time, data = self.protocol.parse_packet(buffer, self.categories)
         
         if self.protocol.is_code_warning(code):
@@ -308,7 +200,43 @@ class TunnelClient:
             except BaseException as e:
                 logger.error("Exception while processing packet %s" % (str(e)), exc_info=True)
                 logger.error("Error packet: %s" % self.read_buffer)
-        self.write_txrx(code)
+        else:
+            self.write_txrx(code)
+
+    def poll_socket(self):
+        readable, writable, exceptional = select.select(self.inputs, self.outputs, self.inputs, self.packet_read_timeout)
+        print("inputs:", self.inputs)
+        print("readable:", readable)
+        print("exceptional:", exceptional)
+        for stream in readable:
+            if stream is self.device:
+                logger.debug("Reading from socket")
+                self.read_buffer += stream.recv(1024)
+                logger.debug("Read buffer: %s" % repr(self.read_buffer))
+        for stream in exceptional:
+            self.inputs.remove(stream)
+            if stream in self.outputs:
+                self.outputs.remove(stream)
+            stream.close()
+            self.should_stop = True
+            logger.error("Exception occurred while reading socket")
+    
+    def read(self, n):
+        length = min(n, len(self.read_buffer))
+        data = self.read_buffer[0:length]
+        self.read_buffer = self.read_buffer[length:]
+        return data
+
+    def readline(self):
+        data = b''
+        char = b''
+        begin_time = time.time()
+        while char != self.protocol.PACKET_STOP:
+            if time.time() - begin_time > self.packet_read_timeout:
+                break
+            char = self.read(1)
+            data += char
+        return data
 
     def read_task_fn(self):
         self.prev_packet_time = time.time()
@@ -319,8 +247,8 @@ class TunnelClient:
                 logger.info("Exiting read thread")
                 return
 
-            while self.is_available():
-                self.read()
+            self.poll_socket()
+            self.read_packet()
 
     def set_start_time(self, device_time):
         self.device_start_time = time.time()
