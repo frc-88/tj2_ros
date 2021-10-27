153b483c24115c429081beed4fb029f7c29a4859
Woz4tetra <woz4tetra@gmail.com>
2021-10-27 02:05:07 -0400

    tj2_tunnel is working. All basic functionality added

--- tj2_tunnel/src/tj2_tunnel/tj2_tunnel.cpp
+++ tj2_tunnel/src/tj2_tunnel/tj2_tunnel.cpp
@@ -9,6 +9,25 @@ TJ2Tunnel::TJ2Tunnel(ros::NodeHandle* nodehandle) :
     ros::param::param<double>("~remote_linear_units_conversion", _remote_linear_units_conversion, 0.3048);
     ros::param::param<double>("~remote_angular_units_conversion", _remote_angular_units_conversion, M_PI / 180.0);
 
+    ros::param::param<bool>("~publish_odom_tf", _publish_odom_tf, true);
+    ros::param::param<string>("~base_frame", _base_frame, "base_link");
+    ros::param::param<string>("~odom_frame", _odom_frame, "odom");
+
+    ros::param::param<double>("~cmd_vel_timeout", _cmd_vel_timeout_param, 0.5);
+    ros::param::param<double>("~min_linear_x_cmd", _min_linear_x_cmd, 0.05);
+    ros::param::param<double>("~min_linear_y_cmd", _min_linear_y_cmd, 0.05);
+    ros::param::param<double>("~min_angular_z_cmd", _min_angular_z_cmd, 0.1);
+    ros::param::param<double>("~zero_epsilon", _zero_epsilon, 0.001);
+
+    _cmd_vel_timeout = ros::Duration(_cmd_vel_timeout_param);
+
+    string key;
+    if (!ros::param::search("categories", key)) {
+        ROS_ERROR("Failed to find categories parameter");
+        return;
+    }
+    nh.getParam(key, _categories_param);
+
     // _categories_param is a map
     if (_categories_param.getType() != XmlRpc::XmlRpcValue::Type::TypeStruct ||
         _categories_param.size() == 0) {
@@ -18,11 +37,16 @@ TJ2Tunnel::TJ2Tunnel(ros::NodeHandle* nodehandle) :
     
     for (XmlRpc::XmlRpcValue::iterator it = _categories_param.begin(); it != _categories_param.end(); ++it)
     {
+        if (it->second.getType() != XmlRpc::XmlRpcValue::TypeString) {
+            ROS_WARN("%s category format isn't a string", it->first.c_str());
+            continue;
+        }
         string category = it->first;
         string format = it->second;
         _categories[category] = format;
     }
 
+    _write_buffer = new char[TunnelProtocol::MAX_PACKET_LEN];
     _read_buffer = new char[READ_BUFFER_LEN];
     _socket_initialized = false;
     _socket_id = 0;
@@ -51,29 +75,226 @@ TJ2Tunnel::TJ2Tunnel(ros::NodeHandle* nodehandle) :
         return;
     }
 
+    _socket_timeout.tv_sec = 0;
+    _socket_timeout.tv_usec = 10000;
+    
     _unparsed_index = 0;
 
+    _prev_ping_time = ros::Time(0);
+    _ping_interval = ros::Duration(1.0);
+
+    _ping_pub = nh.advertise<std_msgs::Float64>("ping", 50);
+
+    _odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 50);
+    _odom_msg.header.frame_id = _odom_frame;
+    _odom_msg.child_frame_id = _base_frame;
+    /* [
+        1e-3, 0.0, 0.0, 0.0, 0.0, 0.0,
+        0.0, 1e-3, 0.0, 0.0, 0.0, 0.0,
+        0.0, 0.0, 1e-3, 0.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 1e-3, 0.0, 0.0,
+        0.0, 0.0, 0.0, 0.0, 1e-3, 0.0,
+        0.0, 0.0, 0.0, 0.0, 0.0, 1e-3
+    ] */
+    /* [
+         0,  1,  2,  3,  4,  5,
+         6,  7,  8,  9, 10, 11,
+        12, 13, 14, 15, 16, 17,
+        18, 19, 20, 21, 22, 23,
+        24, 25, 26, 27, 28, 29,
+        30, 31, 32, 33, 34, 35
+    ] */
+    // odom_msg.pose.covariance.resize(36);
+    _odom_msg.pose.covariance[0] = 5e-2;
+    _odom_msg.pose.covariance[7] = 5e-2;
+    _odom_msg.pose.covariance[14] = 5e-2;
+    _odom_msg.pose.covariance[21] = 5e-2;
+    _odom_msg.pose.covariance[28] = 5e-2;
+    _odom_msg.pose.covariance[35] = 5e-2;
+
+    // odom_msg.twist.covariance.resize(36);
+    _odom_msg.twist.covariance[0] = 10e-2;
+    _odom_msg.twist.covariance[7] = 10e-2;
+    _odom_msg.twist.covariance[14] = 10e-2;
+    _odom_msg.twist.covariance[21] = 10e-2;
+    _odom_msg.twist.covariance[28] = 10e-2;
+    _odom_msg.twist.covariance[35] = 10e-2;
+
+    _twist_sub = nh.subscribe<geometry_msgs::Twist>("cmd_vel", 50, &TJ2Tunnel::twistCallback, this);
+    _prev_twist_timestamp = ros::Time(0);
+    _twist_cmd_speed = 0.0;
+    _twist_cmd_dir = 0.0;
+    _twist_cmd_vt = 0.0;
+
+    _ping_timer = nh.createTimer(ros::Duration(0.5), &TJ2Tunnel::pingCallback, this);
+
+    _poll_socket_thread = new boost::thread(&TJ2Tunnel::pollSocketTask, this);
+
     _socket_initialized = true;
-    ROS_INFO("tj2_networktables init complete");
+    ROS_INFO("tj2_tunnel init complete");
 }
 
 void TJ2Tunnel::packetCallback(PacketResult* result)
 {
     string category = result->getCategory();
-    if (category.compare("odom")) {
-        
+    if (category.compare("odom") == 0) {
+        publishOdom(
+            result->getRecvTime(),
+            result->get_double(0),
+            result->get_double(1),
+            result->get_double(2),
+            result->get_double(3),
+            result->get_double(4),
+            result->get_double(5)
+        );
     }
-    else if (category.compare("ping")) {
-        
+    else if (category.compare("ping") == 0) {
+        std_msgs::Float64 msg;
+        double ping_time = result->get_double(0);
+        double dt = getLocalTime() - ping_time;
+        ROS_DEBUG("Publishing ping time: %f. (Return time: %f)", dt, ping_time);
+        msg.data = dt;
+        _ping_pub.publish(msg);
     }
 }
 
+double TJ2Tunnel::getLocalTime() {
+    return ros::Time::now().toSec();
+}
 
-bool TJ2Tunnel::loop()
+void TJ2Tunnel::publishOdom(ros::Time recv_time, double x, double y, double t, double vx, double vy, double vt)
+{
+    x *= _remote_linear_units_conversion;
+    y *= _remote_linear_units_conversion;
+    t *= _remote_angular_units_conversion;
+    vx *= _remote_linear_units_conversion;
+    vy *= _remote_linear_units_conversion;
+    vt *= _remote_angular_units_conversion;
+
+    tf2::Quaternion quat;
+    quat.setRPY(0, 0, t);
+
+    geometry_msgs::Quaternion msg_quat = tf2::toMsg(quat);
+
+    _odom_msg.header.stamp = recv_time;
+    _odom_msg.pose.pose.position.x = x;
+    _odom_msg.pose.pose.position.y = y;
+    _odom_msg.pose.pose.orientation = msg_quat;
+
+    _odom_msg.twist.twist.linear.x = vx;
+    _odom_msg.twist.twist.linear.y = vy;
+    _odom_msg.twist.twist.angular.z = vt;
+
+    if (_publish_odom_tf)
+    {
+        geometry_msgs::TransformStamped tf_stamped;
+        tf_stamped.header.stamp = recv_time;
+        tf_stamped.header.frame_id = _odom_frame;
+        tf_stamped.child_frame_id = _base_frame;
+        tf_stamped.transform.translation.x = x;
+        tf_stamped.transform.translation.y = y;
+        tf_stamped.transform.translation.z = 0.0;
+        tf_stamped.transform.rotation = msg_quat;
+
+        _tf_broadcaster.sendTransform(tf_stamped);
+    }
+    
+    _odom_pub.publish(_odom_msg);
+}
+
+
+void TJ2Tunnel::twistCallback(const geometry_msgs::TwistConstPtr& msg)
+{
+    double vx = -msg->linear.x;
+    double vy = -msg->linear.y;
+    double vt = -msg->angular.z;
+
+    if (_zero_epsilon < abs(vx) && abs(vx) < _min_linear_x_cmd) {
+        vx = _min_linear_x_cmd;
+    }
+    if (_zero_epsilon < abs(vy) && abs(vy) < _min_linear_y_cmd) {
+        vy = _min_linear_y_cmd;
+    }
+    if (_zero_epsilon < abs(vt) && abs(vt) < _min_angular_z_cmd) {
+        vt = _min_angular_z_cmd;
+    }
+    
+    if (abs(vx) < _zero_epsilon) {
+        vx = 0.0;
+    }
+    if (abs(vy) < _zero_epsilon) {
+        vy = 0.0;
+    }
+    if (abs(vt) < _zero_epsilon) {
+        vt = 0.0;
+    }
+    
+    _prev_twist_timestamp = ros::Time::now();
+    _twist_cmd_speed = sqrt(vx * vx + vy * vy) / _remote_linear_units_conversion;
+    _twist_cmd_dir = fmod(atan2(vy, vx), 2 * M_PI) / _remote_angular_units_conversion;
+    _twist_cmd_vt = vt / _remote_angular_units_conversion;
+}
+
+void TJ2Tunnel::publishCmdVel()
+{
+    ros::Duration dt = ros::Time::now() - _prev_twist_timestamp;
+    if (dt > _cmd_vel_timeout) {
+        // ROS_DEBUG_THROTTLE(2.0, "cmd_vel timed out skipping write.");
+        return;
+    }
+
+    writePacket("cmd", "fff", _twist_cmd_dir, _twist_cmd_speed, _twist_cmd_vt);
+}
+
+
+void TJ2Tunnel::pingCallback(const ros::TimerEvent& event)
+{
+    writePacket("ping", "f", getLocalTime());
+}
+
+// void TJ2Tunnel::publishPing()
+// {
+//     ros::Time now = ros::Time::now();
+//     if (now - _prev_ping_time > _ping_interval) {
+//         writePacket("ping", "f", getLocalTime());
+//         _prev_ping_time = now;
+//     }
+// }
+
+
+void TJ2Tunnel::writePacket(string category, const char *formats, ...)
+{
+    va_list args;
+    va_start(args, formats);
+    _write_lock.lock();
+    int length = protocol->makePacket(_write_buffer, category, formats, args);
+    ROS_DEBUG("Writing packet: %s", protocol->packetToString(_write_buffer, 0, length).c_str());
+    if (length > 0) {
+        write(_socket_id, _write_buffer, length);
+    }
+    else {
+        ROS_DEBUG("Skipping write for packet: %s. Length is %d", protocol->packetToString(_write_buffer, 0, length).c_str(), length);
+    }
+    _write_lock.unlock();
+    va_end(args);
+}
+
+
+bool TJ2Tunnel::pollSocket()
 {
     if (!_socket_initialized) {
         ROS_WARN("Socket is not initialized. Exiting.");
         return false;
+    }
+    FD_ZERO(&_socket_set);  // clear the set
+    FD_SET(_socket_id, &_socket_set); // add our file descriptor to the set
+    int return_val = select(_socket_id + 1, &_socket_set, NULL, NULL, &_socket_timeout);
+    if (return_val == -1) {
+        ROS_ERROR("An error occurred while checking the socket for available data");
+        return false;
+    }
+    else if (return_val == 0) {
+        return true;  // a timeout occurred
     }
     int num_chars_read = read(_socket_id, _read_buffer, READ_BUFFER_LEN);
     if (num_chars_read == -1) {
@@ -81,15 +302,6 @@ bool TJ2Tunnel::loop()
         return false;
     }
     int last_parsed_index = protocol->parseBuffer(_read_buffer, 0, _unparsed_index + num_chars_read);
-    if (last_parsed_index > 0) {
-        for (int index = last_parsed_index, shifted_index = 0; index < READ_BUFFER_LEN; index++, shifted_index++) {
-            _read_buffer[shifted_index] = _read_buffer[index];
-        }
-    }
-    _unparsed_index = _unparsed_index + num_chars_read - last_parsed_index;
-    if (_unparsed_index >= READ_BUFFER_LEN) {
-        _unparsed_index = 0;
-    }
 
     PacketResult* result;
     do {
@@ -103,7 +315,40 @@ bool TJ2Tunnel::loop()
         }
         packetCallback(result);
     }
-    while (result->getErrorCode() != -1);
+    while (result->getErrorCode() != TunnelProtocol::NULL_ERROR);
+
+    _unparsed_index = _unparsed_index + num_chars_read - last_parsed_index;
+    if (_unparsed_index >= READ_BUFFER_LEN) {
+        _unparsed_index = 0;
+    }
+
+    if (last_parsed_index > 0) {
+        for (int index = last_parsed_index, shifted_index = 0; index < READ_BUFFER_LEN; index++, shifted_index++) {
+            _read_buffer[shifted_index] = _read_buffer[index];
+        }
+    }
+
+    return true;
+}
+
+void TJ2Tunnel::pollSocketTask()
+{
+    while (ros::ok())
+    {
+        if (!pollSocket()) {
+            break;
+        }
+    }
+    close(_socket_id);
+}
+
+bool TJ2Tunnel::loop()
+{
+    // if (!pollSocket()) {
+    //     return false;
+    // }
+    publishCmdVel();
+    // publishPing();
     return true;
 }
 
@@ -129,5 +374,6 @@ int TJ2Tunnel::run()
             break;
         }
     }
+    _poll_socket_thread->join();
     return exit_code;
 }
--- tj2_tunnel/src/tunnel_protocol.cpp
+++ tj2_tunnel/src/tunnel_protocol.cpp
@@ -4,6 +4,7 @@
 TunnelProtocol::TunnelProtocol(std::map<string, string> categories)
 {
     _read_packet_num = -1;
+    _write_packet_num = 0;
     _read_buffer_index = 0;
     _categories = categories;
 }
@@ -13,6 +14,100 @@ TunnelProtocol::~TunnelProtocol()
 
 }
 
+int TunnelProtocol::makePacket(char* write_buffer, string category, const char *formats, va_list args)
+{
+    int buffer_index = 0;
+    write_buffer[buffer_index++] = PACKET_START_0;
+    write_buffer[buffer_index++] = PACKET_START_1;
+    buffer_index += 2;  // bytes 2 and 3 are for packet length which will be calculated later
+
+    uint32_union_t union_packet_num;
+    union_packet_num.integer = _write_packet_num;
+    for (unsigned short i = 0; i < 4; i++) {
+        write_buffer[buffer_index++] = union_packet_num.byte[3 - i];
+    }
+    sprintf(write_buffer + buffer_index, "%s", category.c_str());
+    buffer_index += category.length();
+    write_buffer[buffer_index++] = '\t';
+
+    while (*formats != '\0')
+    {
+        if (*formats == 'd') {
+            int32_union_t union_data;
+            union_data.integer = va_arg(args, int32_t);
+            for (unsigned short i = 0; i < sizeof(int32_t); i++) {
+                write_buffer[buffer_index++] = union_data.byte[sizeof(int32_t) - i - 1];
+            }
+        }
+        else if (*formats == 'u') {
+            uint32_union_t union_data;
+            union_data.integer = va_arg(args, uint32_t);
+            for (unsigned short i = 0; i < sizeof(uint32_t); i++) {
+                write_buffer[buffer_index++] = union_data.byte[sizeof(uint32_t) - i - 1];
+            }
+        }
+        else if (*formats == 's') {
+            char *s = va_arg(args, char*);
+            uint16_union_t union_str_len;
+            union_str_len.integer = (uint16_t)strlen(s);
+            for (unsigned short i = 0; i < sizeof(uint16_t); i++) {
+                write_buffer[buffer_index++] = union_str_len.byte[sizeof(uint16_t) - i - 1];
+            }
+            sprintf(write_buffer + buffer_index, "%s", s);
+            buffer_index += strlen(s);
+        }
+        else if (*formats == 'x') {
+            char *s = va_arg(args, char*);
+            uint16_union_t union_str_len;
+            union_str_len.byte[1] = s[0];
+            union_str_len.byte[0] = s[1];
+            if (union_str_len.integer > MAX_SEGMENT_LEN) {
+                ROS_ERROR("Packet segment is too long. Category: %s", category.c_str());
+                return -1;
+            }
+            write_buffer[buffer_index++] = s[0];
+            write_buffer[buffer_index++] = s[1];
+            for (size_t i = 0; i < union_str_len.integer; i++) {
+                write_buffer[buffer_index++] = s[2 + i];
+            }
+        }
+        else if (*formats == 'f') {
+            double_union_t union_data;
+            union_data.floating_point = (double)va_arg(args, double);
+            for (unsigned short i = 0; i < sizeof(double); i++) {
+                write_buffer[buffer_index++] = union_data.byte[i];
+            }
+        }
+        else {
+            ROS_ERROR("Invalid format type. Category: %s", category.c_str());
+            return -1;
+        }
+        ++formats;
+    }
+    uint8_t calc_checksum = 0;
+    for (size_t index = 4; index < buffer_index; index++) {
+        calc_checksum += (uint8_t)write_buffer[index];
+    }
+
+    sprintf(write_buffer + buffer_index, "%02x", calc_checksum);
+    buffer_index += 2;
+    write_buffer[buffer_index++] = '\n';
+    write_buffer[buffer_index] = '\0';
+
+    uint16_t packet_len = buffer_index - 5;  // subtract start, length, and stop bytes
+
+    // insert packet length
+    uint16_union_t union_packet_len;
+    union_packet_len.integer = packet_len;
+    write_buffer[2] = union_packet_len.byte[1];
+    write_buffer[3] = union_packet_len.byte[0];
+
+    _write_packet_num++;
+
+    return buffer_index;
+}
+
+
 int TunnelProtocol::parseBuffer(char* buffer, int start_index, int stop_index)
 {
     int last_packet_index = 0;
@@ -20,7 +115,7 @@ int TunnelProtocol::parseBuffer(char* buffer, int start_index, int stop_index)
     for (index = start_index; index < stop_index; index++) {
         int packet_start = index;
         if (buffer[index] != PACKET_START_0) {
-            ROS_DEBUG("Index %d is not PACKET_START_0", index);
+            // ROS_DEBUG("Index %d is not PACKET_START_0", index);
             continue;
         }
         index++;
@@ -29,7 +124,7 @@ int TunnelProtocol::parseBuffer(char* buffer, int start_index, int stop_index)
             continue;
         }
         if (buffer[index] != PACKET_START_1) {
-            ROS_DEBUG("Index %d is not PACKET_START_1", index);
+            // ROS_DEBUG("Index %d is not PACKET_START_1", index);
             continue;
         }
         index++;
@@ -38,23 +133,21 @@ int TunnelProtocol::parseBuffer(char* buffer, int start_index, int stop_index)
             continue;
         }
 
-        uint16_union_t union_packet_len;
-        union_packet_len.byte[1] = buffer[index++];
-        union_packet_len.byte[0] = buffer[index++];
-        uint16_t length = union_packet_len.integer;
+        uint16_t length = to_uint16(buffer + index);
+        index += 2;
 
         if (index >= stop_index) {
+            // ROS_DEBUG("Buffer length exceeded while searching for length start");
             index = stop_index;
-            ROS_DEBUG("Buffer length exceeded while searching for length start");
             continue;
         }
         
-        ROS_DEBUG("Found packet length: %d", length);
+        // ROS_DEBUG("Found packet length: %d", length);
         index += length;
 
         if (index > stop_index) {
-            index = stop_index;
             ROS_DEBUG("Buffer length exceeded while searching for length stop. %d > %d", index, stop_index);
+            index = stop_index;
             continue;
         }
         if (buffer[index] != PACKET_STOP) {
@@ -63,7 +156,7 @@ int TunnelProtocol::parseBuffer(char* buffer, int start_index, int stop_index)
         }
         index++;
         last_packet_index = index;
-        ROS_DEBUG("Found a packet: %s", packetToString(buffer, packet_start, index).c_str());
+        // ROS_DEBUG("Found a packet: %s", packetToString(buffer, packet_start, index).c_str());
         PacketResult* result = parsePacket(buffer, packet_start, index);
         _result_queue.push_back(result);
     }
@@ -73,6 +166,9 @@ int TunnelProtocol::parseBuffer(char* buffer, int start_index, int stop_index)
 
 PacketResult* TunnelProtocol::popResult()
 {
+    if (_result_queue.size() == 0) {
+        return new PacketResult(NULL_ERROR, ros::Time::now());
+    }
     PacketResult* result = _result_queue.at(0);
     _result_queue.erase(_result_queue.begin());
     return result;
@@ -109,13 +205,13 @@ PacketResult* TunnelProtocol::parsePacket(char* buffer, int start_index, int sto
         return new PacketResult(PACKET_TOO_SHORT_ERROR, recv_time);
     }
 
-    if (buffer[0] != PACKET_START_0) {
+    if (buffer[_read_buffer_index] != PACKET_START_0) {
         ROS_INFO("Packet does not start with PACKET_START_0: %s", packetToString(buffer, start_index, stop_index).c_str());
         _read_packet_num++;
         return new PacketResult(PACKET_0_ERROR, recv_time);
     }
     _read_buffer_index++;
-    if (buffer[1] != PACKET_START_1) {
+    if (buffer[_read_buffer_index] != PACKET_START_1) {
         ROS_INFO("Packet does not start with PACKET_START_1: %s", packetToString(buffer, start_index, stop_index).c_str());
         _read_packet_num++;
         return new PacketResult(PACKET_1_ERROR, recv_time);
@@ -157,7 +253,7 @@ PacketResult* TunnelProtocol::parsePacket(char* buffer, int start_index, int sto
     PacketResult* result = new PacketResult(NO_ERROR, recv_time);
 
     if (recv_packet_num != _read_packet_num) {
-        ROS_WARN("Received packet num doesn't match local count. recv %d != local %d", recv_packet_num, _read_packet_num);
+        ROS_WARN("Received packet num doesn't match local count. recv %d != local %d. %s", recv_packet_num, _read_packet_num, packetToString(buffer, start_index, stop_index).c_str());
         _read_packet_num++;
         _read_packet_num = recv_packet_num;
         result->setErrorCode(PACKET_COUNT_NOT_SYNCED_ERROR);
@@ -186,9 +282,9 @@ PacketResult* TunnelProtocol::parsePacket(char* buffer, int start_index, int sto
     result->setCategory(category);
 
     std::map<std::string, string>::iterator it = _categories.find(category);
-    if (it == _categories.end()) {
+    if (it == _categories.end()) { 
         ROS_WARN(
-            "'%s' is not a recognized category: %s",
+            "'%s' is not a recognized category. Buffer: %s",
             category.c_str(),
             packetToString(buffer, start_index, stop_index).c_str()
         );
@@ -210,8 +306,10 @@ PacketResult* TunnelProtocol::parsePacket(char* buffer, int start_index, int sto
             return new PacketResult(INVALID_FORMAT_ERROR, recv_time);
         }
     }
+    result->setBuffer(buffer);
     result->setErrorCode(NO_ERROR);
     _read_packet_num++;
+    ROS_DEBUG("Parsed packet: %s", packetToString(buffer, start_index, stop_index).c_str());
     
     return result;
 }
@@ -275,7 +373,7 @@ bool TunnelProtocol::parseNextSegment(char* buffer, int stop_index, char format,
         return false;
     }
 
-    result->add(buffer + _current_segment_start);
+    result->add(_current_segment_start, _current_segment_stop - _current_segment_start);
 
     if (format != 'd' && format != 'u' && format != 's' && format != 'x' && format != 'f') {
         ROS_ERROR("Failed to parse segment '%s' as '%c'", packetToString(buffer, _current_segment_start, _current_segment_stop).c_str(), format);
