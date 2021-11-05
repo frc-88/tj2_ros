b57cdcde16e34d458ed488e5cee69626eb139e67
Woz4tetra <woz4tetra@gmail.com>
2021-11-04 20:51:30 -0400

    adding rosbag_to_video script

--- tj2_detectnet/scripts/model_training/test.sh
+++ tj2_detectnet/scripts/model_training/test.sh
@@ -8,8 +8,8 @@ MODEL=$MODEL_DIR/tj2-2020-mb2-ssd-lite.onnx
 
 LABELS=$MODEL_DIR/labels.txt
 # INPUT=/dev/video0
-INPUT=/home/ben/Diff-Swerve-ROS/tj2_detectnet/scripts/video_dataset/output/realsense_2021-10-23-18-05-29.mp4
-# INPUT=/home/ben/Diff-Swerve-ROS/tj2_detectnet/scripts/video_dataset/output/realsense_2021-10-23-18-05-29/realsense_2021-10-23-18-05-29_25.png
+# INPUT=/home/ben/Diff-Swerve-ROS/tj2_detectnet/scripts/video_dataset/output/realsense_2021-10-23-18-05-29.mp4
+INPUT=/home/ben/Diff-Swerve-ROS/tj2_detectnet/scripts/video_dataset/output/realsense_2021-10-23-18-05-29/realsense_2021-10-23-18-05-29_25.png
 # INPUT=/home/ben/tensorflow_workspace/2020Game/data/videos/5172_POV-Great_Northern_2020_Quals_22.mp4
 #INPUT=/home/ben/tensorflow_workspace/2020Game/data/videos/5172_POV-Great_Northern_2020_Quals_60.mp4
 #INPUT=/home/ben/tensorflow_workspace/2020Game/data/videos/Monterrey_Regional_2020_Practice15.mp4
@@ -18,8 +18,8 @@ INPUT=/home/ben/Diff-Swerve-ROS/tj2_detectnet/scripts/video_dataset/output/reals
 #INPUT=/home/ben/object-recognition/detectnet_training/data/2021_Game_Objects_Training_Data/tj2_02-27-2021_2.mp4
 #INPUT=/home/ben/jetson-inference/python/training/detection/ssd/data/tj2_2020_voc_image_database/JPEGImages/tj2_02-27-2021_2_100.jpg
 
-OUTPUT=display://0
-# OUTPUT=/home/ben/Desktop/output.png
+# OUTPUT=display://0
+OUTPUT=/home/ben/Desktop/output.png
 #OUTPUT=/home/ben/Desktop/output.mp4
 
 THRESHOLD=0.5
