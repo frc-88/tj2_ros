80e033101bd81bb20f13a0463ef5e452928c172f
Woz4tetra <woz4tetra@gmail.com>
2021-11-24 11:37:34 -0500

    fixing rtabmap launch

--- commands/laser_slam.sh
+++ commands/laser_slam.sh
@@ -9,9 +9,10 @@ if [ $? != 0 ]; then
     tmux new -s $SESSION -d
 fi
 
-tmux send -t $SESSION "${BASE_DIR}/start_launch.sh rplidar" ENTER
+tmux send -t $SESSION "${BASE_DIR}/start_launch.sh rplidar" ENTER 
 if [ -z ${MAP_NAME} ]; then
-    tmux send -t $SESSION "roslaunch tj2_laser_slam laser_slam.launch" ENTER
+    tmux send -t $SESSION "roslaunch tj2_laser_slam tj2_laser_slam.launch" ENTER 
 else
-    tmux send -t $SESSION "roslaunch tj2_laser_slam laser_slam.launch map_name:=$MAP_NAME mode:=localize" ENTER
+    tmux send -t $SESSION "roslaunch tj2_laser_slam tj2_laser_slam.launch map_name:=$MAP_NAME mode:=localize" ENTER 
 fi
+echo "Started laser slam"
