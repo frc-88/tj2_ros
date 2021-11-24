b4a0061aa7e90ee8166b293f7014230dd9af60e8
Woz4tetra <woz4tetra@gmail.com>
2021-11-24 11:15:58 -0500

    adding more helpful commands

---
+++ commands/rtabmap.sh
@@ -0,0 +1,19 @@
+BASE_DIR=$(realpath "$(dirname $0)")
+
+SESSION=rtabmap
+DB_PATH=$1
+
+tmux has-session -t $SESSION > /dev/null
+
+if [ $? != 0 ]; then
+    tmux new -s $SESSION -d
+fi
+
+tmux send -t $SESSION "${BASE_DIR}/start_launch.sh rplidar
+${BASE_DIR}/start_camera.sh" ENTER
+
+if [ -z ${MAP_NAME} ]; then
+    tmux send -t $SESSION "roslaunch tj2_rtabmap tj2_rtabmap.launch use_laser:=true" ENTER
+else
+    tmux send -t $SESSION "roslaunch tj2_rtabmap tj2_rtabmap.launch use_laser:=true database_path:=$DB_PATH" ENTER
+fi
