11eeea4f388f41c5d5599d30e22571580c2eccbc
Woz4tetra <woz4tetra@gmail.com>
2021-11-24 11:15:58 -0500

    adding more helpful commands

---
+++ commands/laser_slam.sh
@@ -0,0 +1,17 @@
+BASE_DIR=$(realpath "$(dirname $0)")
+
+SESSION=slam
+MAP_NAME=$1
+
+tmux has-session -t $SESSION > /dev/null
+
+if [ $? != 0 ]; then
+    tmux new -s $SESSION -d
+fi
+
+tmux send -t $SESSION "${BASE_DIR}/start_launch.sh rplidar" ENTER
+if [ -z ${MAP_NAME} ]; then
+    tmux send -t $SESSION "roslaunch tj2_laser_slam laser_slam.launch" ENTER
+else
+    tmux send -t $SESSION "roslaunch tj2_laser_slam laser_slam.launch map_name:=$MAP_NAME mode:=localize" ENTER
+fi
