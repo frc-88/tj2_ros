c8a4b4f5b56cfd132a0178a1d67bfb4964cf860e
Woz4tetra <woz4tetra@gmail.com>
2021-12-14 02:36:55 -0500

    adding limelight files for detectnet node

---
+++ dataset_builder/limelight_targets/export.sh
@@ -0,0 +1,15 @@
+
+MODEL_DIR=model
+#MODEL_NAME=mb2-ssd-lite-Epoch-116-Loss-3.3456220626831055.pth
+MODEL_NAME=""  # picks lowest loss
+OUTPUT=limelight-mb2-ssd-lite.onnx
+
+BASE_NET=mb2-ssd-lite
+
+JETSON_INFERENCE_SSD_DIR=/home/ben/object-recognition/jetson-inference/python/training/detection/ssd
+
+/usr/bin/python3 JETSON_INFERENCE_SSD_DIR/onnx_export.py \
+        --net=$BASE_NET \
+        --model-dir$MODEL_DIR \
+        --input=$MODEL_DIR/$MODEL_NAME \
+        --output=$MODEL_DIR/$OUTPUT
