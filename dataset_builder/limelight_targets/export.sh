67ff1fc5621d2b97fe273a9a4420adacb2db2433
Woz4tetra <woz4tetra@gmail.com>
2021-12-14 03:40:53 -0500

    adjust parameters for limelight

--- dataset_builder/limelight_targets/export.sh
+++ dataset_builder/limelight_targets/export.sh
@@ -1,15 +1,15 @@
 
 MODEL_DIR=model
-#MODEL_NAME=mb2-ssd-lite-Epoch-116-Loss-3.3456220626831055.pth
-MODEL_NAME=""  # picks lowest loss
+MODEL_NAME=mb2-ssd-lite-Epoch-60-Loss-1.8050242725171541.pth
+# MODEL_NAME=""  # picks lowest loss
 OUTPUT=limelight-mb2-ssd-lite.onnx
 
 BASE_NET=mb2-ssd-lite
 
 JETSON_INFERENCE_SSD_DIR=/home/ben/object-recognition/jetson-inference/python/training/detection/ssd
 
-/usr/bin/python3 JETSON_INFERENCE_SSD_DIR/onnx_export.py \
+/usr/bin/python3 $JETSON_INFERENCE_SSD_DIR/onnx_export.py \
         --net=$BASE_NET \
-        --model-dir$MODEL_DIR \
-        --input=$MODEL_DIR/$MODEL_NAME \
+        --model-dir=$MODEL_DIR \
+        --input=$MODEL_NAME \
         --output=$MODEL_DIR/$OUTPUT
