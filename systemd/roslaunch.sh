419abc5b137f68093921c86365e0139b5730dd1a
Woz4tetra <woz4tetra@gmail.com>
2021-12-15 17:23:17 -0500

    adding tmux session logging

--- systemd/roslaunch.sh
+++ systemd/roslaunch.sh
@@ -9,12 +9,11 @@ if [ $? != 0 ]; then
     tmux new -s $SESSION -d
 fi
 
-tmux send -t $SESSION "source /home/${USER}/packages_ros_ws/devel/setup.bash
-source /home/${USER}/noetic_ws/install_isolated/setup.bash
-source /home/${USER}/ros_ws/devel/setup.bash
-source /usr/local/bin/env.sh
+tmux send -t $SESSION "source /home/${USER}/packages_ros_ws/devel/setup.bash" ENTER
+tmux send -t $SESSION "source /home/${USER}/noetic_ws/install_isolated/setup.bash" ENTER
+tmux send -t $SESSION "source /home/${USER}/ros_ws/devel/setup.bash" ENTER
+tmux send -t $SESSION ". /usr/local/bin/env.sh roslaunch" ENTER
 
-export ROS_HOME=/home/${USER}/.ros
-export DISPLAY=:0
-roslaunch --wait tj2_bringup tj2_bringup.launch --screen
-" Enter
+tmux send -t $SESSION "export ROS_HOME=/home/${USER}/.ros"
+tmux send -t $SESSION "export DISPLAY=:0"
+tmux send -t $SESSION "roslaunch --wait tj2_bringup tj2_bringup.launch --screen"
