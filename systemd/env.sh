6880bc7cf4b0e5de90dc55b7945a5ed57e2de20d
Woz4tetra <woz4tetra@gmail.com>
2021-12-15 17:23:17 -0500

    adding tmux session logging

--- systemd/env.sh
+++ systemd/env.sh
@@ -7,16 +7,13 @@ TIMEOUT=${1:-900}
 
 stop_time=$((SECONDS+TIMEOUT))
 prev_time=$((SECONDS))
+INTERFACE_NAME=eth0
 
 while true; do
-    HOST_MACHINE=`ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+    HOST_MACHINE=`ifconfig $INTERFACE_NAME | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
     if [[ $HOST_MACHINE =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
         break
     fi
-    # HOST_MACHINE=`ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-    # if [ ! -z ${HOST_MACHINE} ]; then
-    #     break
-    # fi
     sleep 0.5
     if (( $(echo "$SECONDS - $prev_time > $JUMP_THRESHOLD" |bc -l) )); then  # check if time jumped like when the jetson connects to the internet
         prev_time=$((SECONDS))
@@ -33,7 +30,18 @@ done
 
 export ROS_IP=${HOST_MACHINE}
 export ROS_MASTER_URI=http://${HOST_MACHINE}:11311
-# export ROS_MASTER_URI=http://${HOST_MACHINE_NAME}:11311
 
 echo ${ROS_IP}
 echo ${ROS_MASTER_URI}
+
+LOGPREFIX={$1:-log}
+if [[ $TERM = "screen" ]] && [[ $(ps -p $PPID -o comm=) = tmux* ]]; then
+    LOGPREFIX={$1:-log}
+    echo "Enabling tmux logging for $LOGPREFIX"
+    LOGDIR=$HOME/ros-logs
+    mkdir $LOGDIR 2> /dev/null
+    LOGNAME="$LOGPREFEX-$(date '+%Y-%m-%dT%H-%M-%S').log"
+    script -f $LOGDIR/${LOGNAME}
+    exit
+fi
+
--- systemd/roscore.sh
+++ systemd/roscore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /home/${USER}/noetic_ws/install_isolated/setup.bash
-source /usr/local/bin/env.sh
+source /usr/local/bin/env.sh roscore
 roscore & while ! echo exit | nc localhost 11311 > /dev/null; do sleep 1; done
