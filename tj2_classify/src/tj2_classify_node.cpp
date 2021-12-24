4a825a9074360743e0759546d80d58b030e20a22
Woz4tetra <woz4tetra@gmail.com>
2021-12-24 14:38:05 -0500

    adding start of tj2_classify

---
+++ tj2_classify/src/tj2_classify_node.cpp
@@ -0,0 +1,13 @@
+
+#include "tj2_classify/tj2_classify.h"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "tj2_classify");
+    ros::NodeHandle nh;
+
+    TJ2DetectNet broadcaster(&nh);
+    int err = broadcaster.run();
+
+    return err;
+}
