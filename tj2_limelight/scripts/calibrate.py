9698d06f0aab3f51e287779a1650eb8e01f94222
Woz4tetra <woz4tetra@gmail.com>
2022-01-05 20:48:11 -0500

    redoing limelight camera calibration

--- tj2_limelight/scripts/calibrate.py
+++ tj2_limelight/scripts/calibrate.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import os
 import glob
+import yaml
+import time
 
 # Defining the dimensions of checkerboard
 CHECKERBOARD = (5, 8)
@@ -20,6 +22,8 @@ objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
+show_images = True
+
 # Extracting path of individual image stored in a given directory
 images = glob.glob('./images/*.jpg')
 for fname in images:
@@ -29,11 +33,10 @@ for fname in images:
     # If desired number of corners are found in the image then ret = true
     ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + cv2.CALIB_CB_NORMALIZE_IMAGE)
     
-    """
-    If desired number of corner are detected,
-    we refine the pixel coordinates and display 
-    them on the images of checker board
-    """
+    # If desired number of corner are detected,
+    # we refine the pixel coordinates and display 
+    # them on the images of checker board
+
     if ret == True:
         objpoints.append(objp)
         # refining pixel coordinates for given 2d points.
@@ -44,27 +47,67 @@ for fname in images:
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
     
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
+    if show_images:
+        cv2.imshow('img',img)
+        key = chr(cv2.waitKey(15) & 0xff)
+        if key == 'q':
+            quit()
 
-cv2.destroyAllWindows()
+if show_images:
+    cv2.destroyAllWindows()
 
 h,w = img.shape[:2]
 
-"""
-Performing camera calibration by 
-passing the value of known 3D points (objpoints)
-and corresponding pixel coordinates of the 
-detected corners (imgpoints)
-"""
+
+# Performing camera calibration by 
+# passing the value of known 3D points (objpoints)
+# and corresponding pixel coordinates of the 
+# detected corners (imgpoints)
+
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+proj_mtx = np.append(mtx, np.zeros((3, 1)), axis=1)
 
 print("Camera matrix : \n")
 print(mtx)
 print("dist : \n")
 print(dist)
-print("rvecs : \n")
-print(rvecs)
-print("tvecs : \n")
-print(tvecs)
+# print("rvecs : \n")
+# print(rvecs)
+# print("tvecs : \n")
+# print(tvecs)
+print("proj : \n")
+print(proj_mtx)
 
+
+assert mtx.shape == (3, 3)
+assert dist.shape == (1, 5)
+
+config = dict(
+    image_width=320,
+    image_height=240,
+    camera_name="limelight",
+    camera_matrix=dict(
+        rows=3,
+        cols=3,
+        data=mtx.flatten().tolist()
+    ),
+    distortion_model="plumb_bob",
+    distortion_coefficients=dict(
+        rows=1,
+        cols=5,
+        data=dist.flatten().tolist()
+    ),
+    rectification_matrix=dict(
+        rows=3,
+        cols=3,
+        data=np.eye(3).flatten().tolist()
+    ),
+    projection_matrix=dict(
+        rows=3,
+        cols=4,
+        data=proj_mtx.flatten().tolist()
+    )
+)
+
+with open("320x240.yaml", 'w') as file:
+    yaml.dump(config, file)
