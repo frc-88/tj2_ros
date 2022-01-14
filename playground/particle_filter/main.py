624cf5fbc654538782103a216d2c1e2ced9de134
Woz4tetra <woz4tetra@gmail.com>
2022-01-14 14:08:09 -0500

    moving measurement velocities to input

--- playground/particle_filter/main.py
+++ playground/particle_filter/main.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np
 from jit_particle_filter import JitParticleFilter
 from particle_filter import ParticleFilter, FilterSerial
 from state import State, OBJECT_NAMES
@@ -18,36 +19,82 @@ class DeltaTimer:
         return dt
 
 
+class VelocityFilter:
+    def __init__(self, k):
+        self.k = k
+        self.prev_value = None
+        self.speed = 0.0
+
+    def update(self, dt, value):
+        if self.prev_value is None:
+            self.prev_value = value
+        raw_delta = (value - self.prev_value) / dt
+        self.prev_value = value
+        if self.k is None:
+            self.speed = raw_delta
+        else:
+            self.speed = self.k * (raw_delta - self.speed)
+        return self.speed
+
+
 class DeltaMeasurement:
     def __init__(self):
-        self.prev_state = None
+        smooth_k = None
+        self.vx_filter = VelocityFilter(smooth_k)
+        self.vy_filter = VelocityFilter(smooth_k)
+        self.vz_filter = VelocityFilter(smooth_k)
         self.timer = DeltaTimer()
 
-    def update(self, stamp, state):
-        if self.prev_state is None:
-            self.prev_state = state
-            return state
-        dt = self.timer.dt(stamp)
+    def update(self, state: State):
+        dt = self.timer.dt(state.stamp)
         if dt == 0.0:
             return state
         new_state = State.from_state(state)
-        new_state.vx = (state.x - self.prev_state.x) / dt
-        new_state.vy = (state.y - self.prev_state.y) / dt
-        new_state.vz = (state.z - self.prev_state.z) / dt
-        self.prev_state = state
+        new_state.vx = self.vx_filter.update(dt, state.x)
+        new_state.vy = self.vy_filter.update(dt, state.y)
+        new_state.vz = self.vz_filter.update(dt, state.z)
         return new_state
+
+
+class InputVector:
+    def __init__(self):
+        self.odom_timer = DeltaTimer()
+        # self.meas = {}
+        self.meas = DeltaMeasurement()
+        self.meas_input = np.array([0.0, 0.0, 0.0, 0.0])
+        self.vector = np.array([0.0, 0.0, 0.0, 0.0])
+
+    def odom_update(self, odom_state: State):
+        dt = self.odom_timer.dt(odom_state.stamp)
+        self.vector = np.array([odom_state.vx, odom_state.vy, odom_state.vz, odom_state.vt])
+        self.vector += self.meas_input
+        return dt
+
+    def meas_update(self, meas_state: State):
+        # TODO: add trackers for each instance of an object
+        # name = meas_state.type
+        # if name not in self.meas:
+        #     self.meas[name] = DeltaMeasurement()
+        # self.meas[name].update(meas_state)
+        new_state = self.meas.update(meas_state)
+        self.meas_input = np.array([new_state.vx, new_state.vy, new_state.vz, new_state.vt])
+
+    def get_vector(self):
+        return self.vector
 
 
 def main():
     repickle = False
     # path = "./data/detections_2022-01-14-12-02-32.json"
-    path = "./data/detections_2022-01-14-12-39-34.json"
+    # path = "./data/detections_2022-01-14-12-39-34.json"
+    # path = "./data/detections_2022-01-14-13-42-43.json"
+    path = "./data/detections_2022-01-14-14-05-17.json"
 
     states = state_loader.read_pkl(path, repickle)
 
-    meas_std_val = 0.004
-    u_std = [0.004, 0.004, 0.004, 0.004]
-    initial_range = [1.0, 1.0, 1.0, 0.25, 0.25, 0.25]
+    meas_std_val = 0.05
+    u_std = [0.007, 0.007, 0.007, 0.007]
+    initial_range = [1.0, 1.0, 1.0]
     ground_plane = 0.1
 
     is_grounded = True
@@ -60,11 +107,10 @@ def main():
     plotter = ParticleFilterPlotter3D(x_width, y_width, z_width)
     # plotter = ParticleFilterPlotter2D(x_width, y_width)
 
-    d_meas = {}
-    timer = DeltaTimer()
-
     sim_start_t = states[0].stamp
     real_start_t = time.time()
+
+    input_u = InputVector()
 
     for state in states:
         current_time = time.time()
@@ -79,17 +125,16 @@ def main():
         sim_duration = sim_time - sim_start_t
         real_duration = real_time - real_start_t
         if state.type == "odom":
-            input_u = [state.vx, state.vy, state.vz, state.vt]
             if not pf.is_initialized():
                 continue
-            pf.predict(input_u, timer.dt(state.stamp), is_grounded)
+            dt = input_u.odom_update(state)
+            vector = input_u.get_vector()
+            pf.predict(vector, dt, is_grounded)
             plotter.update_odom(state)
         elif state.type in OBJECT_NAMES:
-            if state.type not in d_meas:
-                d_meas[state.type] = DeltaMeasurement()
-            state = d_meas[state.type].update(state.stamp, state)
-            meas_z = [state.x, state.y, state.z, state.vx, state.vy, state.vz]
-            print(("%0.3f\t" * 3) % tuple(meas_z[3:6]))
+            input_u.meas_update(state)
+            meas_z = [state.x, state.y, state.z]
+            print(("%0.3f\t" * len(meas_z)) % tuple(meas_z))
 
             if not pf.is_initialized():
                 print("initializing with %s" % meas_z)
