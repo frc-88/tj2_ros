c82f19f3c90305b1213f873df85211c0eb4c92c4
Woz4tetra <woz4tetra@gmail.com>
2022-03-16 21:42:41 -0400

    got map origin to rotate

--- playground/field_map_util/main.py
+++ playground/field_map_util/main.py
@@ -30,25 +30,26 @@ def load_map(map_name):
     return (map_image, map_config)
 
 
-def rotate_image(image, angle_radians, rotate_center, fill_value):
-    # rotate_matrix = cv2.getRotationMatrix2D(rotate_center, math.degrees(angle_radians), 1.0)
-    # rotate_I = np.eye(3)
-    # rotate_I[0:2] = rotate_matrix[0:2]
-    # xmin, ymin, xmax, ymax = 0, 0, image.shape[0], image.shape[1]
-    # pt1 = np.array([xmin, ymin, 1], dtype=np.int32)
-    # pt2 = np.array([xmax, ymin, 1], dtype=np.int32)
-    # pt3 = np.array([xmin, ymax, 1], dtype=np.int32)
-    # pt4 = np.array([xmax, ymax, 1], dtype=np.int32)
-    # pts = np.array([pt1, pt2, pt3, pt4])
-    # rotated_dims = np.dot(pts, rotate_I.T)
-    # rotated_dims = rotated_dims.astype(np.int32)
-    # new_shape = np.array([
-    #     np.max(rotated_dims[:, 0]) - np.min(rotated_dims[:, 0]),
-    #     np.max(rotated_dims[:, 1]) - np.min(rotated_dims[:, 1])
-    # ], dtype=np.int32)
-    # result = cv2.warpAffine(image, rotate_matrix, image.shape[1::-1], borderValue=fill_value, flags=cv2.INTER_LINEAR)
-    # return result
+def rotate_with_warp(image, angle_radians, rotate_center, fill_value):
+    rotate_matrix = cv2.getRotationMatrix2D(rotate_center, math.degrees(angle_radians), 1.0)
+    rotate_I = np.eye(3)
+    rotate_I[0:2] = rotate_matrix[0:2]
+    xmin, ymin, xmax, ymax = 0, 0, image.shape[0], image.shape[1]
+    pt1 = np.array([xmin, ymin, 1], dtype=np.int32)
+    pt2 = np.array([xmax, ymin, 1], dtype=np.int32)
+    pt3 = np.array([xmin, ymax, 1], dtype=np.int32)
+    pt4 = np.array([xmax, ymax, 1], dtype=np.int32)
+    pts = np.array([pt1, pt2, pt3, pt4])
+    rotated_dims = np.dot(pts, rotate_I.T)
+    rotated_dims = rotated_dims.astype(np.int32)
+    new_shape = np.array([
+        np.max(rotated_dims[:, 0]) - np.min(rotated_dims[:, 0]),
+        np.max(rotated_dims[:, 1]) - np.min(rotated_dims[:, 1])
+    ], dtype=np.int32)
+    result = cv2.warpAffine(image, rotate_matrix, image.shape[1::-1], borderValue=fill_value, flags=cv2.INTER_LINEAR)
+    return result
 
+def rotate_image(image, angle_radians, fill_value):
     return scipy.ndimage.rotate(image, math.degrees(angle_radians), cval=fill_value)
 
 
@@ -64,44 +65,49 @@ def load_waypoints(map_name):
         waypoints[name] = pose
     return waypoints
 
-def pose_to_pixel_params(x, y, origin_x, origin_y, resolution, map_shape):
-    return (
-        int((x - origin_x) / resolution),
-        map_shape[0] - int((y - origin_y) / resolution)
-    )
 
 def pose_to_pixel(pose: Pose2d, map_config, map_shape):
-    return pose_to_pixel_params(pose.x, pose.y, map_config["origin"][0], map_config["origin"][1], map_config["resolution"], map_shape)
-
-
-def pixel_to_pose_params(x, y, origin_x, origin_y, resolution, map_shape):
-    return Pose2d(
-        resolution * float(x) + origin_x,
-        resolution * (float(map_shape[0]) + origin_y - float(y))
+    origin = Pose2d(*map_config["origin"])
+    pose_image_relative = pose.rotate_by(origin.theta)
+    pose_image_relative = pose_image_relative.delta(origin)
+    return (
+        int(pose_image_relative.x / map_config["resolution"]),
+        map_shape[0] - int(pose_image_relative.y / map_config["resolution"])
     )
 
 
 def pixel_to_pose(xy, map_config, map_shape):
-    return pixel_to_pose_params(xy[0], xy[1], map_config["origin"][0], map_config["origin"][1], map_config["resolution"], map_shape)
+    return Pose2d(
+        map_config["resolution"] * float(xy[0]) + map_config["origin"][0],
+        map_config["resolution"] * (float(map_shape[0]) + map_config["origin"][1] - float(xy[1]))
+    )
+
+
+def pose_origin_to_pixel(origin_x, origin_y, map_resolution, map_height):
+    return -origin_x / map_resolution, map_height + origin_y / map_resolution
+
+def pixel_origin_to_pose(pixel_x, pixel_y, map_resolution, map_height):
+    return -pixel_x * map_resolution, (pixel_y - map_height) * map_resolution
 
 
 def pose_to_line(pose: Pose2d, map_config, map_shape, line_length):
     x0, y0 = pose_to_pixel(pose, map_config, map_shape)
+    pose.theta -= map_config["origin"][2]
     y1 = int(line_length * math.sin(pose.theta)) + y0
     x1 = int(line_length * math.cos(pose.theta)) + x0
     return (x0, y0), (x1, y1)
 
 
 def draw_pose(image, name, pose, map_config):
+    pt1, pt2 = pose_to_line(pose, map_config, image.shape, 30)
+    cv2.putText(image, name, pose_to_pixel(pose, map_config, image.shape), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (128, 0, 255))
     cv2.circle(image, pose_to_pixel(pose, map_config, image.shape), 3, (255, 0, 0), -1)
-    pt1, pt2 = pose_to_line(pose, map_config, image.shape, 10)
-    cv2.line(image, pt1, pt2, (255, 0, 0), 2)
-    cv2.putText(image, name, pose_to_pixel(pose, map_config, image.shape), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255, 0, 0))
+    cv2.line(image, pt1, pt2, (128, 0, 0), 2)
 
 
 def draw_waypoints(map_image, map_config, waypoints):
     draw_image = cv2.cvtColor(map_image, cv2.COLOR_GRAY2BGR)
-    draw_pose(draw_image, "origin", Pose2d(), map_config)
+    draw_pose(draw_image, "origin", Pose2d(theta=-map_config["origin"][2]), map_config)
     for name, pose in waypoints.items():
         draw_pose(draw_image, name, pose, map_config)
     return draw_image
@@ -116,10 +122,18 @@ def save_waypoints(waypoints, path):
         yaml.dump(waypoints_config, file)
 
 def get_image_center(image):
-    return (np.array(image.shape[:2][::-1]) - 1) / 2.0
+    return (np.array(image.shape[:2][::-1]) - 1.0) / 2.0
+
+def draw_maps(old_map_image, new_map_image):
+    cv2.imshow("old_map", old_map_image)
+    cv2.imshow("new_map", new_map_image)
+    while True:
+        key = cv2.waitKey(-1)
+        if chr(key & 0xff) == 'q':
+            break
 
 def center_map_on_pose(origin_pose: Pose2d, map_image, map_config, waypoints, new_map_name="{map_name}-centered", new_origin=(0.0, 0.0)):
-    map_name = os.path.basename(map_config["image"])
+    map_name = os.path.splitext(os.path.basename(map_config["image"]))[0]
     new_map_name = new_map_name.format_map(defaultdict(str, map_name=map_name))
     new_map_filename = new_map_name + ".yaml"
     new_image_filename = new_map_name + ".pgm"
@@ -130,27 +144,37 @@ def center_map_on_pose(origin_pose: Pose2d, map_image, map_config, waypoints, ne
     unknown_value = (map_config["occupied_thresh"] + map_config["free_thresh"]) / 2.0
     unknown_value = int(unknown_value * np.iinfo(map_image.dtype).max)
 
-    rotated_image = rotate_image(map_image, -origin_pose.theta, pose_to_pixel(origin_pose, map_config, map_image.shape), unknown_value)
+    image_rotate_angle = -origin_pose.theta
+    rotated_image = rotate_image(map_image, image_rotate_angle, unknown_value)
 
-    pose_center_px = Pose2d(*get_image_center(map_image))
+    map_center_px = Pose2d(*get_image_center(map_image))
     rotate_center_px = Pose2d(*get_image_center(rotated_image))
-    rotate_center_px.theta = origin_pose.theta
+    rotate_center_px.theta = -image_rotate_angle
     map_origin = Pose2d(*map_config["origin"])
-    map_origin_px = Pose2d(*pose_to_pixel_params(map_origin.x, map_origin.y, 0.0, 0.0, map_config["resolution"], map_image.shape))
-    map_origin_px.x = -map_origin_px.x
+    map_origin_px = Pose2d(*pose_origin_to_pixel(map_origin.x, map_origin.y, map_config["resolution"], map_image.shape[0]))
 
-    new_map_origin_px = (map_origin_px.delta(pose_center_px)).relative_to(rotate_center_px)
-    new_map_origin = pixel_to_pose_params(-new_map_origin_px.x, new_map_origin_px.y, 0.0, 0.0, map_config["resolution"], rotated_image.shape)
-    new_map_origin.theta = origin_pose.theta
-    print(map_origin, new_map_origin)
+    new_map_origin_px = (map_origin_px.delta(map_center_px)).relative_to(rotate_center_px)
+    new_map_origin = Pose2d(*pixel_origin_to_pose(new_map_origin_px.x, new_map_origin_px.y, map_config["resolution"], rotated_image.shape[0]))
+    new_map_origin.theta = image_rotate_angle
+
+    # map_image = cv2.cvtColor(map_image, cv2.COLOR_GRAY2BGR)
+    # rotated_image = cv2.cvtColor(rotated_image, cv2.COLOR_GRAY2BGR)
+
+    # cv2.circle(map_image, (int(map_origin_px.x), int(map_origin_px.y)), 5, (255, 0, 0), 3)
+    # cv2.circle(rotated_image, (int(new_map_origin_px.x), int(new_map_origin_px.y)), 5, (255, 0, 0), 3)
+    # draw_maps(map_image, rotated_image)
 
     new_map_config = copy.deepcopy(map_config)
+    # origin_pose.theta = 0.0
+    # new_map_config["origin"] = new_map_origin.relative_to_reverse(origin_pose).to_list()
     new_map_config["origin"] = new_map_origin.to_list()
     new_map_config["image"] = new_image_filename
-    
+
+    # map_transform = new_map_origin.relative_to_reverse(map_origin)
     new_waypoints = {}
     for name, pose in waypoints.items():
-        new_pose = pose.relative_to_reverse(origin_pose)
+        # new_pose = pose.relative_to_reverse(origin_pose)
+        new_pose = pose
         new_pose.x += new_origin[0]
         new_pose.y += new_origin[1]
         new_waypoints[name] = new_pose
@@ -176,13 +200,7 @@ def main():
 
     old_map_image = draw_waypoints(map_image, map_config, waypoints)
     new_map_image = draw_waypoints(rotated_image, new_map_config, new_waypoints)
-
-    cv2.imshow("old_map", old_map_image)
-    cv2.imshow("new_map", new_map_image)
-    while True:
-        key = cv2.waitKey(-1)
-        if chr(key & 0xff) == 'q':
-            break
+    draw_maps(old_map_image, new_map_image)
 
 
 if __name__ == '__main__':
