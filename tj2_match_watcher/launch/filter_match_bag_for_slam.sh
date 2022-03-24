e1e2af53b9f41e5e97e36e9bf1da34aa539708f3
Woz4tetra <woz4tetra@gmail.com>
2022-03-23 22:58:55 -0400

    got bag filter script working

--- tj2_match_watcher/launch/filter_match_bag_for_slam.sh
+++
@@ -1,3 +0,0 @@
-BASE_DIR=$(realpath "$(dirname $0)")
-
-rosbag filter $BASE_DIR/../bags/$1 $BASE_DIR/../bags/$2 "topic in ('/sinistra_laser/scan', '/dextra_laser/scan', '/tj2/odom', '/camera/color/image_raw')"
