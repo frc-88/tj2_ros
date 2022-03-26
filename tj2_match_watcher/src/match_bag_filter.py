0c6371c4ab73a7ea28533540d8fbc1a39b9983ce
Woz4tetra <woz4tetra@gmail.com>
2022-03-25 23:46:46 -0400

    adding some images at a lowered frame rate

--- tj2_match_watcher/src/match_bag_filter.py
+++ tj2_match_watcher/src/match_bag_filter.py
@@ -7,12 +7,10 @@ import rospkg
 from rosbag import Bag
 
 
-def main():
+def main(directory, bag_in_name, time_start=None, time_stop=None):
     rospack = rospkg.RosPack()
 
-    bag_in_name = "2022_robot_2022-03-04-12-51-16.bag"
-
-    bag_in_path = os.path.join(rospack.get_path("tj2_match_watcher"), "bags", "week1", bag_in_name)
+    bag_in_path = os.path.join(rospack.get_path("tj2_match_watcher"), "bags", directory, bag_in_name)
     bag_out_path = os.path.join(
         os.path.dirname(bag_in_path),
         os.path.splitext(os.path.basename(bag_in_path))[0] + "-filter.bag"
@@ -20,9 +18,6 @@ def main():
     
     bag_in = Bag(bag_in_path)
     bag_out = Bag(bag_out_path, 'w')
-
-    time_start = 113
-    time_stop = None
 
     if time_start is not None:
         time_start = rospy.Time(bag_in.get_start_time() + time_start)
@@ -38,6 +33,7 @@ def main():
 
     left_laser_topic = "/left_laser"
     right_laser_topic = "/right_laser"
+    camera_timer = None
     try:
         with tqdm.tqdm(total=length) as pbar:
             for topic, msg, timestamp, conn_header in messages:
@@ -49,7 +45,15 @@ def main():
                     topic = "/dextra_laser" + topic[len(right_laser_topic):]
                     msg.header.frame_id = "dextra_laser_link"
                 if topic.startswith("/camera"):
-                    continue
+                    if topic != "/camera/color/image_raw":
+                        continue
+                    if camera_timer is None:
+                        camera_timer = timestamp
+                        continue
+                    rate = 1.0 / (timestamp - camera_timer).to_sec()
+                    if rate > 15.0:
+                        continue
+                    camera_timer = timestamp
                 if topic.startswith("/move_base"):
                     continue
                 if topic.startswith("/pursuit"):
@@ -71,4 +75,15 @@ def main():
         bag_out.close()
 
 if __name__ == '__main__':
-    main()
+    directory = "week3"
+    
+    bags = {
+        # "2022_robot_2022-03-19-10-12-13.bag": 275,
+        # "2022_robot_2022-03-19-11-47-53.bag": 595,
+        # "2022_robot_2022-03-19-14-03-24.bag": 290,
+        # "2022_robot_2022-03-19-14-51-16.bag": 100,
+        # "2022_robot_2022-03-19-15-23-57.bag": 132,
+        "2022_robot_2022-03-19-15-54-11.bag": 186,
+    }
+    for bag, start_time in bags.items():
+        main(directory, bag, time_start=start_time)
