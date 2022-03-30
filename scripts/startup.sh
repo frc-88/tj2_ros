dd539b53ebfa3b375faaaea060616c7d2161f434
Woz4tetra <woz4tetra@gmail.com>
2022-03-29 21:36:19 -0400

    adjusting AMCL parameters and turret compensation code

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,6 @@
 #export ROS_MAP_NAME=tja
 #export ROS_MAP_NAME=br-114
 #export ROS_MAP_NAME=br-114-03-17-2022-center
-export ROS_MAP_NAME=br-114-03-26-2022
+# export ROS_MAP_NAME=br-114-03-26-2022
+export ROS_MAP_NAME=br-114-03-29-2022
 #export ROS_MAP_NAME=rapid-react-2022-02-19T07-55-53--407688-edited
