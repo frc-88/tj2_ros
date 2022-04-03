ef62cdc4bd67eed7a6db14a4a3466bbb7fa0cf0b
Woz4tetra <woz4tetra@gmail.com>
2022-04-02 21:30:53 -0400

    flipping map correctly for DXF generator. Small laser and slam bug fixes

--- playground/field_map_util/average_center_waypoint.py
+++ playground/field_map_util/average_center_waypoint.py
@@ -7,7 +7,7 @@ parser.add_argument("map_name", help="name of map to get waypoints from")
 parser.add_argument("waypoint_1", help="waypoint_1")
 parser.add_argument("waypoint_2", help="waypoint_2")
 parser.add_argument("zero_theta_waypoint", help="waypoint to use for theta")
-parser.add_argument("new_waypoint_name", help="name of averaged waypoint")
+parser.add_argument("new_waypoint_name", default="center", help="name of averaged waypoint")
 args = parser.parse_args()
 
 waypoint_1 = args.waypoint_1
@@ -20,7 +20,7 @@ waypoints = load_waypoints(map_name)
 average_pose = (waypoints[waypoint_1] + waypoints[waypoint_2]) / 2.0
 average_pose.theta = waypoints[zero_theta_waypoint].theta
 
-waypoints["center"] = average_pose
+waypoints[args.new_waypoint_name] = average_pose
 
 save_waypoints(waypoints, os.path.join(waypoints_dir, map_name + ".yaml"))
 
