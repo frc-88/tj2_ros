6da974b76cf0063e0aa7f1ef1740e6048454923b
Woz4tetra <woz4tetra@gmail.com>
2022-04-13 19:08:16 -0400

    writing target data to csv. added plotting

--- playground/target_utils/limelight_comparison.py
+++ playground/target_utils/limelight_comparison.py
@@ -1,38 +1,18 @@
 #!/usr/bin/python3
-import os
 import csv
 import math
 
-from numpy import concatenate
 import rospy
 import tf2_ros
 import tf2_geometry_msgs
 
-import numpy as np
-
-from scipy.interpolate import interp1d
-
-from std_msgs.msg import Bool
-from std_msgs.msg import String
 from std_msgs.msg import Float64
-
 from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import PoseWithCovarianceStamped
-
 from nav_msgs.msg import Odometry
-from nav_msgs.msg import OccupancyGrid
+from rosgraph_msgs.msg import Clock
 
-from tj2_tools.occupancy_grid import OccupancyGridManager
-
-from tj2_waypoints.msg import WaypointArray
-
-from tj2_networktables.msg import NTEntry
-
-from tj2_target.srv import RecordValue, RecordValueResponse
-from std_srvs.srv import Trigger, TriggerResponse
-
-from tj2_tools.transforms import lookup_transform
 from tj2_tools.robot_state import Pose2d, Velocity
+from tj2_tools.transforms import lookup_transform
 
 
 def meters_to_in(meters):
@@ -47,21 +27,29 @@ class LimelightComparison(object):
             # disable_signals=True
             # log_level=rospy.DEBUG
         )
-        # self.tf_buffer = tf2_ros.Buffer()
-        # self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        rospy.on_shutdown(self.shutdown_hook)
 
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
         self.limelight_angle = 0.0
         self.limelight_dist = 0.0
         self.target_angle = 0.0
         self.target_dist = 0.0
         self.robot_velocity = Velocity()
+        self.current_time = None
+
+        self.file = open("target_data.csv", 'w')
+        self.writer = csv.DictWriter(self.file, fieldnames=("timestamp", "x", "y", "theta", "target_angle", "target_distance", "limelight_angle", "limelight_distance"))
+        self.writer.writeheader()
 
         self.limelight_angle_sub = rospy.Subscriber("/limelight/target_angle", Float64, self.limelight_angle_callback)
         self.limelight_dist_sub = rospy.Subscriber("/limelight/target_distance", Float64, self.limelight_dist_callback)
         self.target_angle_sub = rospy.Subscriber("/tj2/target_angle", Float64, self.target_angle_callback)
         self.target_dist_sub = rospy.Subscriber("/tj2/target_distance", Float64, self.target_dist_callback)
         self.odom_sub = rospy.Subscriber("/tj2/odom", Odometry, self.odom_callback)
+        self.clock_sub = rospy.Subscriber("/clock", Clock, self.clock_callback)
+
         self.limelight_pub = rospy.Publisher("debug_limelight_target", PoseStamped, queue_size=10)
         self.target_pub = rospy.Publisher("debug_target", PoseStamped, queue_size=10)
 
@@ -84,6 +72,35 @@ class LimelightComparison(object):
         self.robot_velocity.y = msg.twist.twist.linear.y
         self.robot_velocity.theta = msg.twist.twist.angular.z
     
+    def clock_callback(self, msg):
+        self.current_time = msg.clock
+    
+    def record_turret(self):
+        if self.current_time is None:
+            return
+        timestamp = self.current_time.to_sec()
+        global_tf = lookup_transform(self.tf_buffer, "map", "base_link")
+        if global_tf is None:
+            return
+
+        zero_pose_base = PoseStamped()
+        zero_pose_base.header.frame_id = "base_link"
+        zero_pose_base.pose.orientation.w = 1.0
+
+        global_pose = tf2_geometry_msgs.do_transform_pose(zero_pose_base, global_tf)
+        global_pose_2d = Pose2d.from_ros_pose(global_pose.pose)
+        row = {
+            "timestamp": timestamp,
+            "x": global_pose_2d.x,
+            "y": global_pose_2d.y,
+            "theta": global_pose_2d.theta,
+            "target_angle": self.target_angle,
+            "target_distance": self.target_dist,
+            "limelight_angle": self.limelight_angle,
+            "limelight_distance": self.limelight_dist,
+        }
+        self.writer.writerow(row)
+
     def run(self):
         clock = rospy.Rate(10.0)
         
@@ -105,6 +122,7 @@ class LimelightComparison(object):
                 target_pose.header.frame_id = "turret_tilt_link"
                 target_pose.pose = target_pose2d.to_ros_pose()
                 self.target_pub.publish(target_pose)
+                self.record_turret()
 
             if self.robot_velocity.distance() < 0.1:
                 limelight_angle_deg = math.degrees(self.limelight_angle)
@@ -113,6 +131,9 @@ class LimelightComparison(object):
                 # print("Limelight (%0.4f) - Waypoint (%0.4f) = %0.4f deg" % (limelight_angle_deg, target_angle_deg, delta))
                 print(delta)
             clock.sleep()
+
+    def shutdown_hook(self):
+        self.file.close()
 
 
 if __name__ == "__main__":
---
+++ playground/target_utils/turret_angle_plotter.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def meters_to_in(meters):
+    return meters * 39.37
+
+
+def in_to_meters(inches):
+    return inches * 0.0254
+
+
+a = 2.69840693e-01
+b = 1.25456722e-04
+c = -1.63681970e+01
+
+def limelight_correction(x):
+    return a * -x * np.log(b * x) + c
+
+
+path = "target_data.csv"
+df = pd.read_csv(path)
+
+ax1 = plt.subplot(2, 1, 1)
+ax2 = plt.subplot(2, 1, 2)
+
+initial_t = df["timestamp"].loc[0]
+timestamps = df["timestamp"] - initial_t
+
+ax1.plot(timestamps, np.degrees(df["target_angle"]), color='b', label="ROS")
+ax1.plot(timestamps, np.degrees(df["limelight_angle"]), color='g', label="limelight")
+ax1.set_xlabel("timestamp")
+ax1.set_ylabel("angle (degrees)")
+ax1.legend()
+
+ax2.plot(timestamps, meters_to_in(df["target_distance"]), color='b', label="ROS")
+ax2.plot(timestamps, meters_to_in(df["limelight_distance"]), color='g', label="limelight")
+ax2.plot(timestamps, limelight_correction(meters_to_in(df["limelight_distance"])), color='limegreen', label="corrected")
+ax2.set_xlabel("timestamp")
+ax2.set_ylabel("distance (inches)")
+ax2.legend()
+plt.show()
