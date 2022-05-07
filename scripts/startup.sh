7bfd0ee80d7d2108b5440d17d731977a89f35a5e
Woz4tetra <woz4tetra@gmail.com>
2022-05-07 02:18:30 -0400

    zed camera. move_base adjustments

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,4 +1,5 @@
-export ROS_MAP_NAME=rapid-react-2022
+# export ROS_MAP_NAME=rapid-react-2022
+export ROS_MAP_NAME=3-101
 export ENABLE_DRIVER_STATION=true
 # export OBJECT_MODEL_NAMES=cargo_2022.names
 export OBJECT_MODEL_NAMES=powercell_2021.names
