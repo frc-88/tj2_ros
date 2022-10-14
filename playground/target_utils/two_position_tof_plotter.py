f67d02c29a4789e009a632cfb089fbfd164b1a2a
Woz4tetra <woz4tetra@gmail.com>
2022-10-14 15:40:48 -0400

    modifying helper script for one table

--- playground/target_utils/two_position_tof_plotter.py
+++ playground/target_utils/two_position_tof_plotter.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import csv
 import rospkg
 import numpy as np
@@ -9,17 +8,22 @@ from scipy.interpolate import interp1d
 from tj2_tools.robot_state import Pose2d
 
 def read_tof_file(path):
-    table = []
+    hood_up_table = []
+    hood_down_table = []
     print("Loading tof table from %s" % path)
     with open(path) as file:
         reader = csv.reader(file)
         header = next(reader)
         for row in reader:
             parsed_row = list(map(float, row[1:]))
-            table.append(parsed_row)
+            if row[0] == "up":
+                hood_up_table.append(parsed_row)
+            elif row[0] == "down":
+                hood_down_table.append(parsed_row)
 
-    table.sort(key=lambda x: x[0])
-    return np.array(table)
+    hood_up_table.sort(key=lambda x: x[0])
+    hood_down_table.sort(key=lambda x: x[0])
+    return np.array(hood_up_table), np.array(hood_down_table)
 
 def read_data_file(path):
     hood_up_table = []
@@ -50,11 +54,7 @@ def fit_curve_fn(x, a, b):
 def fit_tof_data(x_fit, x, y):
     popt, pcov = curve_fit(fit_curve_fn, x, y)
     y_fit = fit_curve_fn(x_fit, *popt)
-    print("---------")
-    print("TOF fit constants:")
-    print(f"tof_a_const = {popt[0]:0.8f}")
-    print(f"tof_b_const = {popt[1]:0.8f}")
-    print("---------")
+    print(popt)
     return y_fit
 
 def create_interp(table):
@@ -69,18 +69,23 @@ def main():
     rospack = rospkg.RosPack()
     tof_path = os.path.join(rospack.get_path("tj2_target"), "config", "time_of_flight.csv")
     recorded_data_path = os.path.join(rospack.get_path("tj2_target"), "config", "recorded_data.csv")
-    table = read_tof_file(tof_path)
+    hood_up_table, hood_down_table = read_tof_file(tof_path)
     # hood_up_table, hood_down_table = read_data_file(recorded_data_path)
-    traj_interp = create_interp(table)
+    traj_interp_up = create_interp(hood_up_table)
+    traj_interp_down = create_interp(hood_down_table)
     x_dist = np.linspace(0.0, 10.0, 100)
-    tof = traj_interp(x_dist)
+    up_tof = traj_interp_up(x_dist)
+    down_tof = traj_interp_down(x_dist)
 
-    fit = fit_tof_data(x_dist, table[:, 0], table[:, 1])
+    hood_up_fit = fit_tof_data(x_dist, hood_up_table[:, 0], hood_up_table[:, 1])
+    hood_down_fit = fit_tof_data(x_dist, hood_down_table[:, 0], hood_down_table[:, 1])
 
-    line = plt.plot(table[:, 0], table[:, 1], '.')[0]
-    if "show" in sys.argv:
-        plt.plot(x_dist, tof, label="TOF from distance")
-        plt.plot(x_dist, fit, label="Fitted TOF")
-        plt.show()
+    line = plt.plot(hood_up_table[:, 0], hood_up_table[:, 1], '.')[0]
+    # plt.plot(x_dist, up_tof, color=line.get_color())
+    plt.plot(x_dist, hood_up_fit, color=line.get_color())
+    line = plt.plot(hood_down_table[:, 0], hood_down_table[:, 1], '.')[0]
+    # plt.plot(x_dist, down_tof, color=line.get_color())
+    plt.plot(x_dist, hood_down_fit, color=line.get_color())
+    plt.show()
 
 main()
