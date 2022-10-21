7bf4f3106804480173c0d0fceeb3ec17e2783d24
Woz4tetra <woz4tetra@gmail.com>
2022-10-20 22:04:50 -0400

    add maps

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 # export ROS_MAP_NAME=rapid-react-2022
 # export ROS_MAP_NAME=3-101
-export ROS_MAP_NAME=br-114-2022-10-20
+export ROS_MAP_NAME=br-114-2022-10-20-edited
 # export ROS_MAP_NAME=tja-2022-09-10
 # export ROS_MAP_NAME=full-garage-2021-10-21T21-18-26--503396-edited
 export ENABLE_DRIVER_STATION=false
