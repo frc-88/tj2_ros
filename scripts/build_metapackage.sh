5157c482d44aeae8459bff0c0fda6385d4ab117c
tj2dev <tjsquareddev@gmail.com>
2022-11-08 14:16:14 -0500

    works on diffyjr

---
+++ scripts/build_metapackage.sh
@@ -0,0 +1,17 @@
+BASE_DIR=$(realpath "$(dirname $0)")
+FOLDER_NAME=$1
+
+CATKIN_WS_PATH=~/ros_ws
+USERNAME=nvidia
+
+cd ${CATKIN_WS_PATH}/src/${FOLDER_NAME}
+
+PACKAGE_LIST=`ls -d */ | sed 's/\///g'`
+PACKAGE_LIST=`echo "$PACKAGE_LIST" | tr '\n' ';'`
+
+cd ${CATKIN_WS_PATH}
+
+export OPENBLAS_CORETYPE=ARMV8
+source /home/${USERNAME}/noetic_ws/install_isolated/setup.bash
+source ${CATKIN_WS_PATH}/devel/setup.bash
+catkin_make -DCATKIN_WHITELIST_PACKAGES="$PACKAGE_LIST"
