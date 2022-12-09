523d4cad0a1a50db1df3b9144806352984088446
Woz4tetra <woz4tetra@gmail.com>
2022-12-09 02:35:44 -0500

    change export to only do ros_ws

--- docker/native/export_container.sh
+++ docker/native/export_container.sh
@@ -6,9 +6,10 @@ if [ "$EUID" -ne 0 ]
     exit
 fi
 
-echo "Exporting tj2_ros image. This will take a very long time."
-mkdir -p /media/storage/docker
-docker save tj2_ros:latest | gzip > /media/storage/docker/tj2_ros.tar.gz
+# echo "Exporting tj2_ros image. This will take a very long time."
+# mkdir -p /media/storage/docker
+# docker save tj2_ros:latest | gzip > /media/storage/docker/tj2_ros.tar.gz
 echo "Compressing docker_ros_ws"
-tar -cvf /media/storage/tj2_ros_ws.tar.gz /media/storage/docker_ros_ws
+cd /media/storage
+tar -cvf tj2_ros_ws.tar.gz docker_ros_ws
 echo "Done!"
--- docker/native/import_container.sh
+++ docker/native/import_container.sh
@@ -8,8 +8,8 @@ fi
 
 cd /media/storage/docker
 
-echo "Importing tj2_ros image. This will take a while."
-docker load -i tj2_ros.tar.gz
+# echo "Importing tj2_ros image. This will take a while."
+# docker load -i tj2_ros.tar.gz
 echo "Decompressing docker_ros_ws"
 tar -xvf tj2_ros_ws.tar.gz
 echo "Done!"
