dc37c271330f68f15f033a5e2d39f73f776d4b52
Woz4tetra <woz4tetra@gmail.com>
2023-01-10 11:39:17 -0500

    fix workstation docker

---
+++ workstation/docker/resources/install/post_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "ros workspace src: ${ROS_WS_SRC}"
+
+cd ${ROS_WS_SRC}
+/root/scripts/clone_ros_packages.sh
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+cd ${ROS_WS_ROOT}
+rosdep install --from-paths src --ignore-src --rosdistro=noetic -y -r || true
+catkin_make -DCATKIN_WHITELIST_PACKAGES="" -DCATKIN_BLACKLIST_PACKAGES="tj2_yolo;zed_nodelets;zed_ros;zed_wrapper"
