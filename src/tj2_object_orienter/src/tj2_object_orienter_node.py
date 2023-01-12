8a39ebaa024c27fdaa6b1e2d5ca2a72da8014532
Woz4tetra <woz4tetra@gmail.com>
2023-01-11 20:44:48 -0500

    fix orienter crash bugs

--- src/tj2_object_orienter/src/tj2_object_orienter_node.py
+++ src/tj2_object_orienter/src/tj2_object_orienter_node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+import copy
 import os
 from typing import List
 import rospy
@@ -6,11 +8,12 @@ import ros_numpy  # apt install ros-noetic-ros-numpy
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-from sensor_msgs.msg import PointCloud2
-from vision_msgs.msg import Detection3DArray, BoundingBox3D
-from geometry_msgs.msg import Pose, Vector3, Quaternion, PoseStamped
+from sensor_msgs.msg import PointCloud2, PointField
+import sensor_msgs.point_cloud2 as pc2
+from vision_msgs.msg import Detection3DArray, Detection3D, BoundingBox3D, ObjectHypothesisWithPose
+from geometry_msgs.msg import Pose, Vector3, Quaternion
 from visualization_msgs.msg import MarkerArray, Marker
-from std_msgs.msg import ColorRGBA
+from std_msgs.msg import ColorRGBA, Header
 
 
 def convert_pc_msg_to_np(pc_msg):
@@ -22,6 +25,39 @@ def convert_pc_msg_to_np(pc_msg):
     # Conversion from PointCloud2 msg to np array.
     pc_np = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(pc_msg, remove_nans=True)
     return pc_np
+
+FIELDS_XYZ = [
+    PointField(name='x', offset=0, datatype=PointField.FLOAT32, count=1),
+    PointField(name='y', offset=4, datatype=PointField.FLOAT32, count=1),
+    PointField(name='z', offset=8, datatype=PointField.FLOAT32, count=1),
+]
+FIELDS_XYZRGB = FIELDS_XYZ + \
+    [PointField(name='rgb', offset=12, datatype=PointField.UINT32, count=1)]
+
+BIT_MOVE_16 = 2**16
+BIT_MOVE_8 = 2**8
+
+def convert_pc_o3d_to_ros(open3d_cloud, frame_id):
+    # Set "header"
+    header = Header()
+    header.stamp = rospy.Time.now()
+    header.frame_id = frame_id
+
+    # Set "fields" and "cloud_data"
+    points=np.asarray(open3d_cloud.points)
+    if not open3d_cloud.colors: # XYZ only
+        fields=FIELDS_XYZ
+        cloud_data=points
+    else: # XYZ + RGB
+        fields=FIELDS_XYZRGB
+        # -- Change rgb color from "three float" to "one 24-byte int"
+        # 0x00FFFFFF is white, 0x00000000 is black.
+        colors = np.floor(np.asarray(open3d_cloud.colors)*255) # nx3 matrix
+        colors = colors[:,0] * BIT_MOVE_16 +colors[:,1] * BIT_MOVE_8 + colors[:,2]  
+        cloud_data=np.c_[points, colors]
+    
+    # create ros_cloud
+    return pc2.create_cloud(header, fields, cloud_data)
 
 
 class TJ2ObjectOrienter:
@@ -43,7 +79,8 @@ class TJ2ObjectOrienter:
         self.max_detection_distance = rospy.get_param("~max_detection_distance", 1.0)
         self.class_names_path = rospy.get_param("~class_names_path", "")
         self.class_names = self.load_class_names(self.class_names_path)
-        self.marker_color = ColorRGBA(1.0, 0.0, 0.0, 1.0)
+        self.marker_color = ColorRGBA(1.0, 0.0, 0.0, 0.5)
+        np.set_printoptions(threshold=np.inf)
 
         self.point_cloud_sub = rospy.Subscriber(
             "points",
@@ -51,6 +88,11 @@ class TJ2ObjectOrienter:
             self.point_cloud_callback,
             queue_size=1,
             buff_size=52428800,
+        )
+        self.debug_point_cloud_pub = rospy.Publisher(
+            "points_debug",
+            PointCloud2,
+            queue_size=1
         )
         self.detections_sub = rospy.Subscriber(
             "detections", Detection3DArray, self.detections_callback, queue_size=10
@@ -61,6 +103,10 @@ class TJ2ObjectOrienter:
         )
 
         self.detection_markers_pub = rospy.Publisher(
+            "detections/markers", MarkerArray, queue_size=10
+        )
+
+        self.oriented_detection_markers_pub = rospy.Publisher(
             "detections/oriented/markers", MarkerArray, queue_size=10
         )
 
@@ -80,6 +126,8 @@ class TJ2ObjectOrienter:
 
         points = convert_pc_msg_to_np(msg)
         pcl = open3d.geometry.PointCloud()
+        oriented_detections = Detection3DArray()
+        oriented_detections.header = self.detections.header
         for detection in self.detections.detections:
             center = np.array(
                 [
@@ -90,23 +138,36 @@ class TJ2ObjectOrienter:
                 dtype=np.float64,
             )
             size = np.array(
-                [detection.bbox.size.x, detection.bbox.size.y, detection.bbox.size.z],
+                [detection.bbox.size.x, detection.bbox.size.y, detection.bbox.size.z - 0.25],
                 dtype=np.float64,
             )
-            lower_point = center - size
-            upper_point = center + size
-            pcl.points = open3d.utility.Vector3dVector(
-                points[lower_point <= points <= upper_point]
-            )
+            size = np.abs(size)
+            lower_point = center - size / 2.0
+            upper_point = center + size / 2.0
+            indices = np.all(np.logical_and(lower_point <= points, points <= upper_point), axis=1)
+            # print(points)
+            print(center)
+            print(size)
+            print(np.count_nonzero(indices))
+            if not np.any(indices):
+                return
+            pcl.points = open3d.utility.Vector3dVector(points[indices])
+            self.debug_point_cloud_pub.publish(convert_pc_o3d_to_ros(pcl, msg.header.frame_id))
             oriented_box = self.get_oriented_box_from_cropped_cloud(pcl)
-            detection.bbox = oriented_box
+            
+            oriented_detection = copy.deepcopy(detection)
+            oriented_detection.header = detection.header
+            oriented_detection.bbox = oriented_box
+            oriented_detection.results[0].pose.pose = oriented_box.center
+            oriented_detections.detections.append(oriented_detection)
+        self.publish_detection_markers(self.oriented_detection_markers_pub, oriented_detections)
 
     def get_oriented_box_from_cropped_cloud(
         self, pcl: open3d.geometry.PointCloud
     ) -> BoundingBox3D:
         o3d_bb = pcl.get_oriented_bounding_box()
         pca_center = o3d_bb.get_center()
-        quat = Rotation.from_matrix(o3d_bb.R @ self.rotate_up).as_quat()
+        quat = Rotation.from_matrix(copy.deepcopy(o3d_bb.R)).as_quat()
         pose = Pose()
         pose.position.x = pca_center[0]
         pose.position.y = pca_center[1]
@@ -119,12 +180,21 @@ class TJ2ObjectOrienter:
 
     def detections_callback(self, msg: Detection3DArray):
         self.detections = msg
+        self.publish_detection_markers(self.detection_markers_pub, self.detections)
 
     def add_detection_to_marker_array(self, marker_array, detection_3d_msg):
         cube_marker = self.make_marker(detection_3d_msg, self.marker_color)
         text_marker = self.make_marker(detection_3d_msg, self.marker_color)
+        arrow_marker = self.make_marker(detection_3d_msg, self.marker_color)
 
         label, count = self.get_detection_label(detection_3d_msg)
+        
+        arrow_marker.type = Marker.ARROW
+        arrow_marker.ns = "arrow_" + cube_marker.ns
+        
+        arrow_marker.scale.x = cube_marker.scale.x / 2.0
+        arrow_marker.scale.y = 0.05
+        arrow_marker.scale.z = 0.05
 
         cube_marker.type = Marker.CUBE
         cube_marker.ns = "cube_" + cube_marker.ns
@@ -132,16 +202,17 @@ class TJ2ObjectOrienter:
         text_marker.type = Marker.TEXT_VIEW_FACING
         text_marker.ns = "text_" + cube_marker.ns
         text_marker.text = "%s_%s|%0.1f" % (
-            label,
+            "cone",
             count,
             detection_3d_msg.results[0].score * 100,
         )
         text_marker.scale.z = min(text_marker.scale.x, text_marker.scale.y)
-        text_marker.scale.x = 0.0
-        text_marker.scale.y = 0.0
+        # text_marker.scale.x = 0.0
+        # text_marker.scale.y = 0.0
 
         marker_array.markers.append(cube_marker)
         marker_array.markers.append(text_marker)
+        marker_array.markers.append(arrow_marker)
 
     def make_marker(self, detection_3d_msg, color):
         marker = Marker()
@@ -153,18 +224,18 @@ class TJ2ObjectOrienter:
         marker.ns = label
         marker.id = count
 
-        marker.scale.x = detection_3d_msg.bbox.size.x
-        marker.scale.y = detection_3d_msg.bbox.size.y
-        marker.scale.z = detection_3d_msg.bbox.size.z
+        marker.scale.x = abs(detection_3d_msg.bbox.size.x)
+        marker.scale.y = abs(detection_3d_msg.bbox.size.y)
+        marker.scale.z = abs(detection_3d_msg.bbox.size.z)
         marker.color = color
 
         return marker
 
-    def publish_detection_markers(self, detections: Detection3DArray):
+    def publish_detection_markers(self, publisher: rospy.Publisher, detections: Detection3DArray):
         markers = MarkerArray()
         for detection in detections.detections:
             self.add_detection_to_marker_array(markers, detection)
-        self.detection_markers_pub.publish(markers)
+        publisher.publish(markers)
 
     def get_detection_label(self, detection_msg):
         return self.get_label(detection_msg.results[0].id)
@@ -177,3 +248,15 @@ class TJ2ObjectOrienter:
         else:
             label = f"?? ({class_index})"
         return label, class_count
+
+    def run(self):
+        rospy.spin()
+
+
+def main():
+    node = TJ2ObjectOrienter()
+    node.run()
+
+
+if __name__ == "__main__":
+    main()
