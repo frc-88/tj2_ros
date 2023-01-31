73d33033743c17403684abdb11e7490998611f07
Woz4tetra <woz4tetra@gmail.com>
2023-01-31 01:20:10 -0500

    add tag particle filter

---
+++ src/tj2_charged_up/scripts/particle_filter/particle_filter.py
@@ -0,0 +1,196 @@
+import warnings
+import numpy as np
+from numpy.random import randn, random, uniform, normal
+import scipy.stats
+from threading import Lock
+
+
+G_CONST = 9.81
+
+
+def predict(particles, input_std_error, num_particles, u, dt):
+    """
+    This is a static function so that it can be fed through numba's "jit" function (see jit_particle_filter.py)
+
+    move according to control input u (velocity of robot and velocity of object)
+    with noise std
+    u[0, 1, 2] = linear_vx, linear_vy, angular_z
+    """
+    x_0 = particles[:, 0]
+    y_0 = particles[:, 1]
+    th_0 = particles[:, 2]
+
+    vx_u = u[0]
+    vy_u = u[1]
+    vt_u = u[2]
+
+    vx_sd_u = input_std_error[0]
+    vy_sd_u = input_std_error[1]
+    vt_sd_u = input_std_error[2]
+
+    # angular predict
+    theta_delta = vt_u * dt + normal(0.0, 1.0, num_particles) * vt_sd_u
+    x_a = x_0 * np.cos(theta_delta) - y_0 * np.sin(theta_delta)
+    y_a = x_0 * np.sin(theta_delta) + y_0 * np.cos(theta_delta)
+
+    # x, y linear predict
+    x_1 = x_a + vx_u * dt + normal(0.0, 1.0, num_particles) * vx_sd_u
+    y_1 = y_a + vy_u * dt + normal(0.0, 1.0, num_particles) * vy_sd_u
+    th_1 = th_0 + theta_delta
+
+    particles[:, 0] = x_1
+    particles[:, 1] = y_1
+    particles[:, 2] = th_1
+
+
+class ParticleFilter:
+    def __init__(self, num_particles, measure_std_error, input_std_error):
+        self.num_states = 3  # x, y, theta
+        self.particles = np.zeros((num_particles, self.num_states))
+        self.num_particles = num_particles
+        self.measure_std_error = measure_std_error
+        self.input_std_error = np.array(input_std_error)
+
+        self.measure_distribution = scipy.stats.norm(0.0, self.measure_std_error)
+        self.weights = np.array([])
+
+        self.initialize_weights()
+
+        self.lock = Lock()
+
+    def reset(self):
+        with self.lock:
+            self.particles = np.zeros((self.num_particles, self.num_states))
+            self.initialize_weights()
+
+    def set_parameters(self, num_particles, measure_std_error, input_std_error):
+        with self.lock:
+            self.num_particles = num_particles
+            self.measure_std_error = measure_std_error
+            self.input_std_error = np.array(input_std_error)
+            
+        self.reset()
+
+    def initialize_particles(self, initial_distribution_type, initial_range):
+        zero_state = np.zeros((self.num_states, 1), dtype=np.float64)
+        if initial_distribution_type == "gaussian":
+            self.create_gaussian_particles(zero_state, initial_range)
+        elif initial_distribution_type == "uniform":
+            self.create_uniform_particles(zero_state, initial_range)
+        else:
+            warnings.warn(f"Invalid distribution type: {initial_distribution_type}. Defaulting to gaussian.")
+            self.create_gaussian_particles(zero_state, initial_range)
+        self.reset()
+
+    def is_initialized(self):
+        with self.lock:
+            return not np.all(self.particles == 0.0)
+
+    def initialize_weights(self):
+        # initialize with uniform weight
+        self.weights = np.ones(self.num_particles)
+        self.weights /= np.sum(self.weights)
+
+    def create_uniform_particles(self, initial_state, state_range):
+        assert len(initial_state) == self.num_states
+        assert len(state_range) == self.num_states
+
+        with self.lock:
+            self.initialize_weights()
+            for state_num in range(self.num_states):
+                min_val = initial_state[state_num] - state_range[state_num]
+                max_val = initial_state[state_num] + state_range[state_num]
+                self.particles[:, state_num] = uniform(min_val, max_val, size=self.num_particles)
+
+    def create_gaussian_particles(self, mean, var):
+        with self.lock:
+            self.initialize_weights()
+            for state_num in range(self.num_states):
+                self.particles[:, state_num] = mean[state_num] + randn(self.num_particles) * var[state_num]
+
+    def predict(self, u, dt):
+        """
+        move according to control input u (velocity of robot and velocity of object)
+        with noise std
+        u[0, 1, 2] = linear_vx, linear_vy, angular_z
+        """
+        with self.lock:
+            predict(self.particles, self.input_std_error, self.num_particles, u, dt)
+
+    def update(self, z):
+        """Update particle filter according to measurement z (robot global position from tag: [x, y, theta])"""
+        # self.weights.fill(1.0)  # debateable as to whether this is detrimental or not (shouldn't weights be preserved between measurements?)
+        with self.lock:
+            distances = np.linalg.norm(self.particles - z, axis=1)
+            
+            self.weights *= self.measure_distribution.pdf(distances)
+
+            self.weights += 1.e-300  # avoid divide by zero error
+            self.weights /= np.sum(self.weights)  # normalize
+
+    def neff(self):
+        with self.lock:
+            return 1.0 / np.sum(np.square(self.weights))
+
+    def resample(self):
+        # indices = self.simple_resample()
+        indices = self.systematic_resample()
+
+        # resample according to indices
+        self.particles = self.particles[indices]
+        self.weights = self.weights[indices]
+        self.weights /= np.sum(self.weights)  # normalize
+
+    def resample_from_index(self, indices):
+        assert len(indices) == self.num_particles
+
+        with self.lock:
+            self.particles = self.particles[indices]
+            self.weights = self.weights[indices]
+            self.weights /= np.sum(self.weights)
+
+    def estimate(self):
+        """ returns mean and variance """
+        mu = self.mean()
+        with self.lock:
+            var = np.average((self.particles - mu) ** 2, weights=self.weights, axis=0)
+
+        return mu, var
+
+    def mean(self):
+        """ returns weighted mean position"""
+        with self.lock:
+            return np.average(self.particles, weights=self.weights, axis=0)
+
+    def get_state(self) -> FilterState:
+        mu = self.mean()
+        return FilterState(mu[0], mu[1], mu[2], 0.0, mu[3], mu[4], mu[5], 0.0)
+
+    def check_resample(self):
+        neff = self.neff()
+        with self.lock:
+            if neff < self.num_particles / 2.0:
+                self.resample()
+                return True
+            else:
+                return False
+
+    def simple_resample(self):
+        cumulative_sum = np.cumsum(self.weights)
+        cumulative_sum[-1] = 1.0  # avoid round-off error
+        indices = np.searchsorted(cumulative_sum, random(self.num_particles))
+        return indices
+
+    def systematic_resample(self):
+        cumulative_sum = np.cumsum(self.weights)
+        indices = np.zeros(self.num_particles, 'int')
+        t = np.linspace(0, 1.0 - 1.0 / self.num_particles, self.num_particles) + random() / self.num_particles
+
+        i, j = 0, 0
+        while i < self.num_particles and j < self.num_particles:
+            while cumulative_sum[j] < t[i]:
+                j += 1
+            indices[i] = j
+            i += 1
+
+        return indices
---
+++ src/tj2_charged_up/scripts/tag_localization.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+
+import rospy
+
+from dynamic_reconfigure.server import Server
+
+import numpy as np
+
+from tf.transformations import quaternion_from_euler
+
+from apriltag_ros.msg import AprilTagDetectionArray, AprilTagDetection
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Pose, PoseArray, PoseStamped, Quaternion
+
+from particle_filter import JitParticleFilter
+from particle_filter import ParticleFilter
+
+from tj2_interfaces.msg import WaypointArray
+from tj2_tools.waypoint import Waypoints2dArray
+from tj2_charged_up.cfg import ParticleFilterConfig
+
+
+class TagLocalizationNode:
+    def __init__(self):
+        self.node_name = "tag_localization"
+        rospy.init_node(
+            self.node_name,
+            # disable_signals=True
+            # log_level=rospy.DEBUG
+        )
+        # rospy.on_shutdown(self.shutdown_hook)
+
+        self.meas_std_val = rospy.get_param("~meas_std_val", 0.03)
+        self.u_std = rospy.get_param("~u_std", [1.0, 1.0, 1.0])
+        self.initial_range = rospy.get_param("~initial_range", [1.0, 1.0, 2 * np.pi])
+        self.num_particles = rospy.get_param("~num_particles", 50)
+        self.initial_distribution_type = rospy.get_param("~initial_distribution_type", "gaussian")
+        self.velocity_smooth_k = rospy.get_param("~velocity_smooth_k", 0.0)
+        self.loop_rate = rospy.get_param("~loop_rate", 15.0)
+        self.particle_filter_type = rospy.get_param("~particle_filter_type", "JitParticleFilter")
+        self.tag_id_to_waypoint_map = rospy.get_param("~tag_id_to_waypoint_map", {})
+        self.global_frame = rospy.get_param("~global_frame", "map")
+
+        if self.particle_filter_type == "JitParticleFilter":
+            self.pf = JitParticleFilter(self.num_particles, self.meas_std_val, self.u_std)
+        elif self.particle_filter_type == "ParticleFilter":
+            self.pf = ParticleFilter(self.num_particles, self.meas_std_val, self.u_std)
+        else:
+            raise RuntimeError(f"Invalid particle filter type: {self.particle_filter_type}")
+        self.pf.initialize_particles(self.u_std, self.initial_distribution_type)
+        
+        self.prev_predict_time = rospy.Time.now()
+        self.waypoints = Waypoints2dArray()
+
+        self.pose_publisher = rospy.Publisher("robot_tag_pose", PoseStamped, queue_size=5)
+        self.detections_sub = rospy.Subscriber("tag_detections", AprilTagDetectionArray, self.detections_callback, queue_size=25)
+        self.odom_sub = rospy.Subscriber("odom", Odometry, self.odom_callback, queue_size=25)
+        self.waypoints_sub = rospy.Subscriber("waypoints", WaypointArray, self.waypoints_callback, queue_size=5)
+
+        self.particles_pub = rospy.Publisher("robot_tag_particles", PoseArray, queue_size=5)
+
+        self.dyn_config = {}
+        self.dyn_server = Server(ParticleFilterConfig, self.dyn_config_callback)
+
+        rospy.loginfo("%s init done" % self.node_name)
+
+    def dyn_config_callback(self, config, level):
+        rospy.loginfo("Updating particle filter dynamic config")
+        if len(self.dyn_config) == 0:
+            config["meas_std_val"] = self.meas_std_val
+            config["u_std_vx"] = self.u_std[0]
+            config["u_std_vy"] = self.u_std[1]
+            config["u_std_vt"] = self.u_std[2]
+            config["num_particles"] = self.num_particles
+            config["initial_distribution_type"] = self.initial_distribution_type
+            self.dyn_config = config
+            return self.dyn_config
+
+        self.meas_std_val = self.get_default_config("meas_std_val", config, self.meas_std_val)
+        self.u_std = [
+            self.get_default_config("u_std_vx", config, self.u_std[0]),
+            self.get_default_config("u_std_vy", config, self.u_std[1]),
+            self.get_default_config("u_std_vt", config, self.u_std[2]),
+        ]
+        self.num_particles = self.get_default_config("num_particles", config, self.num_particles)
+        self.initial_distribution_type = self.get_default_config("initial_distribution_type", config, self.initial_distribution_type)
+        
+        self.pf.set_parameters(self.num_particles, self.meas_std_val, self.u_std)
+        self.pf.initialize_particles(self.u_std, self.initial_distribution_type)
+
+        return self.dyn_config
+    
+    def get_default_config(self, key, new_config, default):
+        if self.dyn_config[key] != new_config[key]:
+            self.dyn_config[key] = new_config[key]
+            rospy.loginfo("Setting %s to %s" % (key, new_config[key]))
+            return new_config[key]
+        else:
+            return default
+
+    def waypoints_callback(self, msg: WaypointArray):
+        self.waypoints = Waypoints2dArray.from_waypoints_array(msg)
+
+    def detections_callback(self, msg):
+        states = []
+        for detection in msg.detections:
+            states.append(self.tag_to_robot_state(detection))
+        weighted_state = np.mean(np.array(states, dtype=np.float64))
+        self.pf.update(weighted_state)
+
+    def tag_to_robot_state(self, detection: AprilTagDetection) -> np.ndarray:
+        # convert detection to pose stamped
+        # lookup tag id in waypoint map
+        # request global waypoint pose
+        # apply robot->tag transform relative to waypoint. get pose
+        # convert pose to particle filter measurement
+        pass
+
+    def odom_callback(self, msg):
+        dt = self.get_predict_dt(msg.header.stamp)
+        self.pf.predict(self.odom_to_predict_vector(msg), dt)
+
+    def get_predict_dt(self, stamp) -> float:
+        # return time since last predict message
+        dt = (stamp - self.prev_predict_time).to_sec()
+        self.prev_predict_time = stamp
+        return dt
+
+    def odom_to_predict_vector(self, msg: Odometry) -> np.ndarray:
+        # assumes velocities are in the robot base_link frame
+        return np.array([
+            msg.twist.twist.linear.x,
+            msg.twist.twist.linear.y,
+            msg.twist.twist.angular.z,
+        ])
+
+    def publish_all_pose(self):
+        pose = self.robot_state_to_pose(self.pf.mean())
+        pose_stamped = PoseStamped()
+        pose_stamped.header.stamp = rospy.Time.now()
+        pose_stamped.header.frame_id = self.global_frame
+        pose_stamped.pose = pose
+        self.pose_publisher.publish(pose_stamped)
+    
+    def robot_state_to_pose(self, state: np.ndarray) -> Pose:
+        pose = Pose()
+        pose.position.x = state[0]
+        pose.position.y = state[1]
+        pose.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, state[2]))
+        return pose
+    
+    def publish_particles(self):
+        if self.particles_pub.get_num_connections() == 0:
+            return
+        particles_msg = PoseArray()
+        particles_msg.header.frame_id = self.global_frame
+        particles_msg.header.stamp = rospy.Time.now()
+
+        for particle in self.pf.particles:
+            particles_msg.poses.append(self.robot_state_to_pose(particle))
+
+        self.particles_pub.publish(particles_msg)
+
+    def run(self):
+        rate = rospy.Rate(self.loop_rate)
+
+        while True:
+            rate.sleep()
+            if rospy.is_shutdown():
+                break
+
+            if self.pf.is_initialized():
+                self.pf.check_resample()
+
+            self.publish_all_pose()
+            self.publish_particles()
+
+
+if __name__ == "__main__":
+    node = TagLocalizationNode()
+    try:
+        node.run()
+    except rospy.ROSInterruptException:
+        pass
+    finally:
+        rospy.loginfo("Exiting %s node" % node.node_name)
