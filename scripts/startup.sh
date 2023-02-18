169de2d2664dd5bc32717faa243e8b54546576e5
tj2dev <tjsquareddev@gmail.com>
2023-02-17 23:34:14 -0500

    fix filters, shortcuts

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -5,6 +5,7 @@ export ROBOT=diffyjr
 # export ROBOT=2022-swervy
 # export ROS_MAP_NAME=rapid-react-2022
 export ROS_MAP_NAME=charged-up-2023
+# export ROS_MAP_NAME=week0-charged-up-2023
 # export ROS_MAP_NAME=br-114-2023-01-21
 # export ROS_MAP_NAME=demo-day
 # export ROS_MAP_NAME=3-101
