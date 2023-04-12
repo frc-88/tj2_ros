6e2af37c60aa741a947b46a9a494acdd0f2824ca
Woz4tetra <woz4tetra@gmail.com>
2023-04-12 03:46:26 -0400

    grid zone implements

--- src/tj2_charged_up/scripts/tj2_charged_up/grid_tracker.py
+++ src/tj2_charged_up/scripts/tj2_charged_up/grid_tracker.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import copy
+import time
 from typing import List, Optional, Tuple
 from threading import Lock
 import rospy
@@ -10,8 +12,11 @@ from std_msgs.msg import ColorRGBA
 from vision_msgs.msg import Detection3DArray
 from geometry_msgs.msg import PoseStamped, Vector3, TransformStamped
 from visualization_msgs.msg import Marker, MarkerArray
+from image_geometry import PinholeCameraModel
+from sensor_msgs.msg import CameraInfo
+from nav_msgs.msg import Odometry
 
-from tj2_tools.robot_state import Pose2d
+from tj2_tools.robot_state import Pose2d, Velocity
 from tj2_tools.transforms import lookup_transform
 
 from grid_zone_manager import Alliance, ColumnType, GridZone, GridZoneManager
@@ -26,11 +31,15 @@ class GridTracker:
         )
         grid_zones_path = rospy.get_param("~grid_zones_path", "grid_zones.csv")
         self.contact_threshold = rospy.get_param("~contact_threshold", 1.0)
+        self.no_fill_timeout = rospy.get_param("~no_fill_timeout", 1.0)
+        self.linear_velocity_threshold = rospy.get_param("~linear_velocity_threshold", 1.0)
+        self.angular_velocity_threshold = rospy.get_param("~angular_velocity_threshold", 1.0)
         self.map_link = rospy.get_param("~global_frame_id", "map")
         self.base_link = rospy.get_param("~base_frame_id", "base_link")
         self.nt_host = rospy.get_param("~nt_host", "10.0.88.2")
 
         self.grid_zones = GridZoneManager.from_file(grid_zones_path)
+        self.camera_model: Optional[PinholeCameraModel] = None
         
         NetworkTables.initialize(server=self.nt_host)
         self.smart_dashboard_table = NetworkTables.getTable("SmartDashboard")
@@ -45,17 +54,24 @@ class GridTracker:
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         self.robot_tf_lock = Lock()
+        self.zones_lock = Lock()
         self.global_tf = TransformStamped()
         self.global_tf.header.frame_id = self.map_link
         self.global_tf.child_frame_id = self.base_link
         self.global_tf.transform.rotation.w = 1.0
+        self.robot_velocity = Velocity()
 
-        self.detections_sub = rospy.Subscriber(
-            "detections", Detection3DArray, self.detections_callback, queue_size=10
-        )
         self.markers_pub = rospy.Publisher(
             "grid_tracking/markers", MarkerArray, queue_size=10
         )
+        self.detections_sub = rospy.Subscriber(
+            "detections", Detection3DArray, self.detections_callback, queue_size=10
+        )
+        self.odom_sub = rospy.Subscriber(
+            "odom", Odometry, self.odom_callback, queue_size=10
+        )
+        self.camera_info_sub = rospy.Subscriber("camera_info", CameraInfo, self.info_callback, queue_size=1)
+        rospy.loginfo("Grid tracker initialized")
 
     def update_global_tf(self) -> None:
         global_tf = lookup_transform(self.tf_buffer, self.map_link, self.base_link)
@@ -63,7 +79,20 @@ class GridTracker:
             with self.robot_tf_lock:
                 self.global_tf = global_tf
 
+    def info_callback(self, msg: CameraInfo) -> None:
+        rospy.loginfo("Got camera model")
+        self.camera_info_sub.unregister()
+        self.camera_model = PinholeCameraModel()
+        self.camera_model.fromCameraInfo(msg)
+
+    def odom_callback(self, msg: Odometry) -> None:
+        self.robot_velocity = Velocity.from_ros_twist(msg.twist.twist)
+
     def detections_callback(self, msg: Detection3DArray) -> None:
+        if self.robot_velocity.magnitude() > self.linear_velocity_threshold:
+            return
+        if abs(self.robot_velocity.theta) > self.angular_velocity_threshold:
+            return 
         assert msg.detections is not None
         if msg.header.frame_id == self.base_link:
             self.update_global_tf()
@@ -72,18 +101,51 @@ class GridTracker:
             global_tf = lookup_transform(self.tf_buffer, self.map_link, msg.header.frame_id)
         if global_tf is None:
             return
-        for detection in msg.detections:
-            detection_pose = PoseStamped()
-            detection_pose.header.frame_id = msg.header.frame_id
-            detection_pose.pose = detection.bbox.center
+        if self.camera_model is not None:
+            reverse_tf = lookup_transform(self.tf_buffer, self.camera_model.tfFrame(), self.map_link)
+        else:
+            reverse_tf = None
 
-            global_pose = tf2_geometry_msgs.do_transform_pose(detection_pose, self.global_tf)
-            nearest_zone, distance = self.grid_zones.get_nearest(
-                global_pose.pose.position.x,
-                global_pose.pose.position.y,
-                global_pose.pose.position.z
-            )
-            nearest_zone.filled = distance < self.contact_threshold
+        with self.zones_lock:
+            if reverse_tf is not None:
+                for zone in self.grid_zones.zones:
+                    zone.set_in_view(self.is_zone_in_view(reverse_tf, zone))
+                    now = time.monotonic()
+                    did_fill_timeout = now - zone.filled_time > self.no_fill_timeout
+                    did_not_in_view_timeout = now - zone.not_in_view_time > self.no_fill_timeout
+                    if zone.in_view and did_fill_timeout and did_not_in_view_timeout:
+                        zone.filled = False
+            for detection in msg.detections:
+                detection_pose = PoseStamped()
+                detection_pose.header.frame_id = msg.header.frame_id
+                detection_pose.pose = detection.bbox.center
+
+                global_pose = tf2_geometry_msgs.do_transform_pose(detection_pose, self.global_tf)
+                nearest_zone, distance = self.grid_zones.get_nearest(
+                    global_pose.pose.position.x,
+                    global_pose.pose.position.y,
+                    global_pose.pose.position.z
+                )
+                nearest_zone.set_filled(distance < self.contact_threshold)
+    
+    def is_zone_in_view(self, reverse_tf: Optional[TransformStamped], zone: GridZone) -> bool:
+        if reverse_tf is None or self.camera_model is None:
+            return False
+        grid_pose = PoseStamped()
+        grid_pose.header.frame_id = self.map_link
+        grid_pose.pose.position.x = zone.x
+        grid_pose.pose.position.y = zone.y
+        grid_pose.pose.position.z = zone.z
+        camera_relative_grid = tf2_geometry_msgs.do_transform_pose(grid_pose, reverse_tf)
+        if camera_relative_grid.pose.position.z < 0.0:
+            return False
+
+        px = self.camera_model.project3dToPixel((
+            camera_relative_grid.pose.position.x,
+            camera_relative_grid.pose.position.y,
+            camera_relative_grid.pose.position.z,
+        ))
+        return 0 < px[0] < self.camera_model.width and 0 < px[1] < self.camera_model.height  # type: ignore
     
     def get_robot_pose(self) -> Optional[Pose2d]:
         zero_pose_base = PoseStamped()
@@ -98,27 +160,28 @@ class GridTracker:
         if robot_pose is None:
             self.status_entry.setString("Invalid robot pose")
             return
-        nearest_zone, distance = self.grid_zones.get_nearest(robot_pose.x, robot_pose.y, 0.0)
-        
-        self.nearest_zone_entry.setDouble(distance)
-        self.nearest_zone_serial_entry.setString(self.get_serial(nearest_zone))
+        with self.zones_lock:
+            nearest_zone, distance = self.grid_zones.get_nearest(robot_pose.x, robot_pose.y, 0.0)
+            self.nearest_zone_entry.setDouble(distance)
+            self.nearest_zone_serial_entry.setString(self.get_serial(nearest_zone))
 
-        row = self.grid_zones.get_row(nearest_zone.row)
-        status = {ColumnType.HIGH: False, ColumnType.MID: False, ColumnType.LOW: False}
-        row_index = nearest_zone.row
-        for zone in row:
-            status[zone.column] = zone.filled
-        for column_type, filled in status.items():
-            self.grid_tracking_column_state.getEntry(column_type.value).setBoolean(filled)
-        self.nearest_zone_row_index.setDouble(row_index)
+            row = self.grid_zones.get_row(nearest_zone.alliance, nearest_zone.row)
+            status = {ColumnType.HIGH: False, ColumnType.MID: False, ColumnType.LOW: False}
+            row_index = nearest_zone.row
+            for zone in row:
+                status[zone.column] = zone.filled
+            for column_type, filled in status.items():
+                self.grid_tracking_column_state.getEntry(column_type.value).setBoolean(filled)
+            self.nearest_zone_row_index.setDouble(row_index)
         self.status_entry.setString("OK")
 
     def publish_grid_state_markers(self) -> None:
         marker_array = MarkerArray()
         assert marker_array.markers is not None
-        for zone in self.grid_zones.zones:
-            markers = self.grid_zone_to_marker(zone)
-            marker_array.markers.extend(markers)
+        with self.zones_lock:
+            for zone in self.grid_zones.zones:
+                markers = self.grid_zone_to_marker(zone)
+                marker_array.markers.extend(markers)
         self.markers_pub.publish(marker_array)
     
     def grid_zone_to_marker(self, grid_zone: GridZone) -> List[Marker]:
@@ -129,12 +192,13 @@ class GridTracker:
         pose.pose.position.z = grid_zone.z
         pose.pose.orientation.w = 1.0
         if grid_zone.filled:
-            color = (0.0, 0.0, 0.0, 1.0)
+            color = (0.0, 0.0, 0.0, 0.95)
         else:
+            alpha = 0.95 if grid_zone.in_view else 0.25
             if grid_zone.alliance == Alliance.BLUE:
-                color = (0.0, 0.0, 1.0, 1.0)
+                color = (0.0, 0.0, 1.0, alpha)
             else:
-                color = (1.0, 0.0, 0.0, 1.0)
+                color = (1.0, 0.0, 0.0, alpha)
         sphere_marker = self.make_marker(self.get_serial(grid_zone), pose, color)
         sphere_marker.ns = "sphere" + sphere_marker.ns
         sphere_marker.type = Marker.SPHERE
@@ -142,9 +206,10 @@ class GridTracker:
         text_marker = self.make_marker(self.get_serial(grid_zone), pose, color)
         text_marker.type = Marker.TEXT_VIEW_FACING
         text_marker.ns = "text" + text_marker.ns
-        text_marker.text = f"{grid_zone.column}-{grid_zone.row} {grid_zone.type}"
+        text_marker.text = f"{grid_zone.column.value}-{grid_zone.row} {grid_zone.type.value}"
         text_marker.scale.x = 0.0
         text_marker.scale.y = 0.0
+        text_marker.scale.z = 0.15
         text_marker.color = ColorRGBA(
             r=1.0,
             g=1.0,
@@ -164,9 +229,9 @@ class GridTracker:
         marker.id = 0  # all waypoint names should be unique
 
         scale_vector = Vector3()
-        scale_vector.x = self.contact_threshold
-        scale_vector.y = self.contact_threshold
-        scale_vector.z = self.contact_threshold
+        scale_vector.x = self.contact_threshold * 2
+        scale_vector.y = self.contact_threshold * 2
+        scale_vector.z = self.contact_threshold * 2
         marker.scale = scale_vector
         marker.color = ColorRGBA(
             r=color[0],
@@ -179,7 +244,7 @@ class GridTracker:
 
     @staticmethod
     def get_serial(zone: GridZone) -> str:
-        return f"{zone.alliance}-{zone.column}-{zone.row}"
+        return f"{zone.alliance.value}-{zone.column.value}-{zone.row}"
     
     def run(self):
         rate = rospy.Rate(10)
--- src/tj2_charged_up/scripts/tj2_charged_up/grid_zone_manager.py
+++ src/tj2_charged_up/scripts/tj2_charged_up/grid_zone_manager.py
@@ -1,5 +1,5 @@
 import csv
-from numba import njit
+import time
 import numpy as np
 from typing import Dict, List, Tuple
 from dataclasses import dataclass
@@ -34,29 +34,25 @@ class GridZone:
     column: ColumnType
     row: RowType
     filled: bool = False
+    filled_time: float = 0.0
+    in_view: bool = False
+    not_in_view_time: float = 0.0
 
     def __post_init__(self):
         for field in dataclasses.fields(self):
             value = getattr(self, field.name)
             if not isinstance(value, field.type):
                 setattr(self, field.name, field.type(value))
+    
+    def set_filled(self, filled: bool) -> None:
+        self.filled = filled
+        if self.filled:
+            self.filled_time = time.monotonic()
 
-@njit
-def jit_get_nearest(coords: np.ndarray, x: float, y: float, z: float) -> Tuple[int, float]:
-    point = np.array([x, y, z], dtype=np.float64)
-    delta = np.abs(coords - point)
-    # https://github.com/numba/numba/issues/2181
-    # distance = np.linalg.norm(delta, axis=1)
-    distance = np.sqrt(delta[:, 0] * delta[:, 0] + delta[:, 1] * delta[:, 1] + delta[:, 2] * delta[:, 2])
-    closest_index = int(np.argmin(distance))
-    return closest_index, float(distance)
-
-def np_get_nearest(coords: np.ndarray, x: float, y: float, z: float) -> Tuple[int, float]:
-    point = np.array([x, y, z], dtype=np.float64)
-    delta = np.abs(coords - point)
-    distance = np.linalg.norm(delta, axis=1)
-    closest_index = int(np.argmin(distance))
-    return closest_index, float(distance)
+    def set_in_view(self, in_view: bool) -> None:
+        self.in_view = in_view
+        if not self.in_view:
+            self.not_in_view_time = time.monotonic()
 
 class GridZoneManager:
     def __init__(self) -> None:
@@ -75,20 +71,21 @@ class GridZoneManager:
                 self.add_zone(zone)
         return self
     
-    def get_column(self, column_type: ColumnType) -> List[GridZone]:
-        return [self.zones[index] for index in self.column_indices[column_type]]
+    def get_column(self, alliance: Alliance, column_type: ColumnType) -> List[GridZone]:
+        return [self.zones[index] for index in self.column_indices[column_type] if self.zones[index].alliance == alliance]
 
-    def get_row(self, row_type: RowType) -> List[GridZone]:
-        return [self.zones[index] for index in self.row_indices[row_type]]
+    def get_row(self, alliance: Alliance, row_type: RowType) -> List[GridZone]:
+        return [self.zones[index] for index in self.row_indices[row_type] if self.zones[index].alliance == alliance]
 
     def add_zone(self, grid_zone: GridZone):
+        zone_index = len(self.zones)
         if grid_zone.row not in self.row_indices:
             self.row_indices[grid_zone.row] = []
-        self.row_indices[grid_zone.row].append(len(self.zones))
+        self.row_indices[grid_zone.row].append(zone_index)
 
         if grid_zone.column not in self.column_indices:
             self.column_indices[grid_zone.column] = []
-        self.column_indices[grid_zone.column].append(len(self.zones))
+        self.column_indices[grid_zone.column].append(zone_index)
 
         self.zones.append(grid_zone)
         coords = np.array([[grid_zone.x, grid_zone.y, grid_zone.z]])
@@ -98,5 +95,9 @@ class GridZoneManager:
             self.coords = np.append(self.coords, coords, axis=0)
 
     def get_nearest(self, x: float, y: float, z: float) -> Tuple[GridZone, float]:
-        index, distance = jit_get_nearest(self.coords, x, y, z)
-        return self.zones[index], distance
+        point = np.array([x, y, z], dtype=np.float64)
+        delta = np.abs(self.coords - point)
+        distances = np.linalg.norm(delta, axis=1)
+        closest_index = int(np.argmin(distances))
+        nearest_zone = self.zones[closest_index]
+        return nearest_zone, float(distances[closest_index])
