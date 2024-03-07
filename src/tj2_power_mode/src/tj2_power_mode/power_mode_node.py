a695f41f5e729eda73874e2b598a6eb4cc3c2df1
Woz4tetra <woz4tetra@gmail.com>
2024-03-06 23:31:34 -0500

    add power mode to practice bot

--- src/tj2_power_mode/src/tj2_power_mode/power_mode_node.py
+++ src/tj2_power_mode/src/tj2_power_mode/power_mode_node.py
@@ -15,14 +15,27 @@ class JetsonPowerMode(IntEnum):
 
 class PowerModeNode:
     def __init__(self) -> None:
-        self.default_mode_id = JetsonPowerMode(rospy.get_param("~default_power_mode", JetsonPowerMode.HIGH_POWER.value))
-        self.launch_trigger = JetsonPowerMode(rospy.get_param("~launch_trigger", JetsonPowerMode.HIGH_POWER.value))
+        self.default_mode_id = self.mode_str_to_mode(
+            rospy.get_param("~default_power_mode", JetsonPowerMode.HIGH_POWER.name),
+        )
+        self.launch_trigger = self.mode_str_to_mode(
+            rospy.get_param("~launch_trigger", JetsonPowerMode.HIGH_POWER.name),
+        )
         self.launch_path = str(rospy.get_param("~launch_path", ""))
+        self.bypass = bool(rospy.get_param("~bypass", False))
+
         self.primary_launcher = LaunchManager(self.launch_path) if self.launch_path else None
         self.current_mode_id = JetsonPowerMode.UNKNOWN
         self.prev_mode_set_time = rospy.Time(0)
         self.mode_set_cooldown = rospy.Duration.from_sec(3.0)
         self.mode_sub = rospy.Subscriber("power_mode", Int32, self.mode_callback, queue_size=1)
+
+    def mode_str_to_mode(self, mode_str: str) -> JetsonPowerMode:
+        try:
+            return JetsonPowerMode[mode_str]
+        except KeyError:
+            rospy.logerr(f"Invalid power mode: {mode_str}. Defaulting to UNKNOWN")
+            return JetsonPowerMode.UNKNOWN
 
     def mode_callback(self, msg: Int32) -> None:
         self.set_power_mode(JetsonPowerMode(msg.data))
@@ -55,7 +68,11 @@ class PowerModeNode:
         os.system(f"sudo /usr/sbin/nvpmodel -m {mode_id.value}")
 
     def run(self):
-        self.set_power_mode(self.default_mode_id)
+        if self.default_mode_id != JetsonPowerMode.UNKNOWN and not self.bypass:
+            self.set_power_mode(self.default_mode_id)
+        if self.bypass and self.primary_launcher:
+            rospy.loginfo("Bypassing power mode setting. Starting primary launcher")
+            self.primary_launcher.start()
         rospy.spin()
 
 
