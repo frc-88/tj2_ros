5297314a7ff499419cac7379bc2b245243afb4a0
Woz4tetra <woz4tetra@gmail.com>
2024-04-09 19:16:36 -0400

    better setup scripts

--- install/udev/setup_arducam_udev_rules.sh
+++ install/udev/setup_arducam_udev_rules.sh
@@ -5,4 +5,7 @@ set -e
 
 sudo cp "${BASE_DIR}/99-arducam.rules" "/etc/udev/rules.d/"
 sudo chmod 777 "/etc/udev/rules.d/99-arducam.rules"
-sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+echo "Arducam udev rules installed"
